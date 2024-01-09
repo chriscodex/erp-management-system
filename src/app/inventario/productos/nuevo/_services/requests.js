772c8ed879906cc57c8af41fa39a3fd13af6a279
Christian <c.espinoza.code@gmail.com>
2024-01-09 17:57:32 -0500

    createProductRequest created

--- src/app/inventario/productos/nuevo/_services/requests.js
+++ src/app/inventario/productos/nuevo/_services/requests.js
@@ -4,8 +4,10 @@ import {
   getAllProveedoresServerUrl,
   getSegmentsByFilterServerUrl,
   getAllAlmacenesServerUrl,
+  createProductClientUrl,
 } from '@/lib/urls';
 import { fetchData } from '@/lib/fetchData';
+import { delay } from '@/lib/utils';
 
 export async function getSegmentByFilterRequest(segmentFilter) {
   try {
@@ -100,3 +102,31 @@ export async function getAllAlmacenesRequest() {
     console.error(error);
   }
 }
+
+/* eslint-disable */
+export async function createProductRequest(product, setLoading) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      setLoading(true);
+      // Simular tiempo de retraso para pruebas en la UI
+      await delay();
+
+      // Obtener los datos del producto
+      const response = await postData(createProductClientUrl, product);
+      if (response?.status !== 201) {
+        setLoading(false);
+        reject(
+          'No se pudo crear el producto: ' + response.response?.data?.error
+        );
+        return;
+      }
+
+      setLoading(false);
+      resolve(response?.response?.data?.payload);
+    } catch (error) {
+      setLoading(false);
+      reject(error);
+    }
+  });
+}
+/* eslint-enable */
