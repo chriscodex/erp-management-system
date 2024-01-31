d5f1466e8eb587431475915a017892b726e8576b
Christian <c.espinoza.code@gmail.com>
2024-01-31 09:09:28 -0500

    toast setup with delay implemented

--- src/components/toast/toastSetup.js
+++ src/components/toast/toastSetup.js
@@ -1,18 +1,20 @@
 import { getDataByDni } from '@/lib/utils';
+import { delay } from '@/lib/utils';
 
-/* eslint-disable */
 /**
  * Función para buscar una persona por DNI.
  * @param {string} dni - El DNI de la persona a buscar.
  * @param {function} setLoading - Función para manejar el estado de carga.
  * @returns {Promise} - Promesa que se resuelve con los datos de la persona.
  */
-export function BusquedaPorDni (dni, setLoading) {
+export function BusquedaPorDni(dni, setLoading) {
+  // eslint-disable-next-line
   return new Promise(async (resolve, reject) => {
     try {
       setLoading(true);
       // Simular tiempo de retraso
-      await new Promise((resolve) => setTimeout(resolve, 1500));
+
+      await delay();
 
       // Obtener los datos de la persona
       const response = await getDataByDni(dni);
@@ -21,7 +23,7 @@ export function BusquedaPorDni (dni, setLoading) {
         reject('No se ha encontrado una persona con ese DNI');
         return;
       }
-      
+
       setLoading(false);
       resolve(response?.data?.payload);
     } catch (error) {
@@ -30,4 +32,3 @@ export function BusquedaPorDni (dni, setLoading) {
     }
   });
 }
-/* eslint-enable */
