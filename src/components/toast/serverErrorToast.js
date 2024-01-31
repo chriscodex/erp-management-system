af17db49046761ad3ac403fe2f017eb1cfc748df
Christian <c.espinoza.code@gmail.com>
2024-01-31 09:06:11 -0500

    server toast error created

---
+++ src/components/toast/serverErrorToast.js
@@ -0,0 +1,8 @@
+import { toast } from 'sonner';
+
+export default function serverErrorToast() {
+  toast.error(
+    'No podemos conectarnos al servidor en este momento. Verifica tu conexión a internet o inténtalo nuevamente en unos minutos. Si el error persiste, ponte en contacto con los desarrolladores.',
+    { duration: 10000 }
+  );
+}
