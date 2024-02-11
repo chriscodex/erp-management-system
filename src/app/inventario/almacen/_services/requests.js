d16b17fa0000f5970e7583c5de1c9f2f6b0e63d1
Christian <c.espinoza.code@gmail.com>
2024-02-11 09:22:01 -0500

    requestGetAllAlmacenes now do it from server

--- src/app/inventario/almacen/_services/requests.js
+++ src/app/inventario/almacen/_services/requests.js
@@ -1,14 +1,17 @@
-import { fetchData } from '@/lib/fetchData';
-import { getAllAlmacenesServerUrl } from '@/lib/urls';
+import { connectDB } from '@/db/mongodb';
+import { AlmacenService } from '@/backend/almacenes/application/almacen.service';
 
 export async function getAllAlmacenesRequest() {
   try {
-    const response = await fetchData(getAllAlmacenesServerUrl);
+    await connectDB();
+    const almacenService = new AlmacenService();
+
+    const response = await almacenService.getAllAlmacenes();
     if (response?.status !== 200) {
       console.log('Error al obtener todos los almacenes');
       return { almacenes: [], status: 500 };
     }
-    const almacenes = response?.data?.payload;
+    const almacenes = response?.payload;
     return { almacenes, status: 200 };
   } catch (error) {
     console.error(error);
