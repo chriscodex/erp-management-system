6ee041317de3f7e04cd7360e5562fbf9fb6d77a2
Christian <c.espinoza.code@gmail.com>
2024-03-26 15:00:20 -0500

    getAllAlmacenes with simplificador

--- src/app/inventario/almacen/_services/requests.js
+++ src/app/inventario/almacen/_services/requests.js
@@ -1,6 +1,6 @@
 import { connectDB } from '@/db/mongodb';
 import { AlmacenService } from '@/backend/almacenes/application/almacen.service';
-
+import { simplificadorParaClientComponent } from '@/lib/utils';
 
 export async function getAllAlmacenesRequestServer() {
   try {
@@ -12,8 +12,12 @@ export async function getAllAlmacenesRequestServer() {
       console.log('Error al obtener todos los almacenes');
       return { almacenes: [], status: response?.status };
     }
+
     const almacenes = response?.payload;
-    return { almacenes, status: 200 };
+    return {
+      almacenes: simplificadorParaClientComponent(almacenes),
+      status: 200,
+    };
   } catch (error) {
     console.error(error);
   }
