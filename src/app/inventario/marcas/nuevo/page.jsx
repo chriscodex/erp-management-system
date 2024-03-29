6d73a8d5328e0f40231148af7d6c9e47f4e66772
Christian <c.espinoza.code@gmail.com>
2024-03-28 20:13:05 -0500

    preview branch 2

--- src/app/inventario/marcas/nuevo/page.jsx
+++ src/app/inventario/marcas/nuevo/page.jsx
@@ -29,7 +29,7 @@ export default async function Page() {
         <Card className="w-full max-w-7xl mx-auto">
           <CardHeader>
             <CardTitle className="text-2xl font-bold">
-              Crear Nueva Marcaa
+              Crear Nueva Marca
             </CardTitle>
           </CardHeader>
           <CardContent>
