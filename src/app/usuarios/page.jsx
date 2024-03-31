8d55e5ef0ef9e8c94f35579c649f12beca8a0203
Christian <c.espinoza.code@gmail.com>
2024-03-31 08:08:43 -0500

    container moved into bradcrumb

--- src/app/usuarios/page.jsx
+++ src/app/usuarios/page.jsx
@@ -18,27 +18,23 @@ export default async function Page() {
   return (
     <>
       <NavbarSimple title="Usuarios">
-        <div className="container mx-auto p-4">
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <div className="flex items-center gap-2">
-                <User2Icon className="h-9 w-9" />
-                <Label className="sm:text-4xl text-xl font-bold">
-                  Usuarios
-                </Label>
-              </div>
-              <Link href="/usuarios/nuevo" className="flex justify-end">
-                <Button>
-                  <Plus />
-                  Agregar Usuario
-                </Button>
-              </Link>
-            </CardHeader>
-            <CardContent>
-              <DataTable columns={columns} data={usersSorted} status={status} />
-            </CardContent>
-          </Card>
-        </div>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <div className="flex items-center gap-2">
+              <User2Icon className="h-9 w-9" />
+              <Label className="sm:text-4xl text-xl font-bold">Usuarios</Label>
+            </div>
+            <Link href="/usuarios/nuevo" className="flex justify-end">
+              <Button>
+                <Plus />
+                Agregar Usuario
+              </Button>
+            </Link>
+          </CardHeader>
+          <CardContent>
+            <DataTable columns={columns} data={usersSorted} status={status} />
+          </CardContent>
+        </Card>
       </NavbarSimple>
     </>
   );
