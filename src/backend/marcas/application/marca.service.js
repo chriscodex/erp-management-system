60ad616b1adf05b8d3f103a5c240c3c90fdcb167
Christian <c.espinoza.code@gmail.com>
2024-04-01 16:03:32 -0500

    marca update service fixed

--- src/backend/marcas/application/marca.service.js
+++ src/backend/marcas/application/marca.service.js
@@ -183,6 +183,21 @@ export class MarcaService {
         }
       }
 
+      // Validar si una marca con ese nombre y en el mismo segmento ya existe
+      if (marca.nombre) {
+        const marcaFound = await this.marcaRepository.getMarcaByData(marca);
+        if (marcaFound && marcaFound?._id !== id) {
+          console.log(
+            'Marca Service: Una marca con el mismo nombre ya existe en el segmento seleccionado'
+          );
+          return {
+            status: 409,
+            payload:
+              'Una marca con el mismo nombre ya existe en el segmento seleccionado',
+          };
+        }
+      }
+
       const marcaUpdated = await this.marcaRepository.updateMarca(id, marca);
 
       if (!marcaUpdated) {
