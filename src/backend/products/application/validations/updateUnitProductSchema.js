ccec78531f639a5792f3fc899c5426c7a29655d8
Christian <c.espinoza.code@gmail.com>
2024-04-03 07:55:16 -0500

    updateUnitProductschema created

---
+++ src/backend/products/application/validations/updateUnitProductSchema.js
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+const objectIdRegex = /^[a-f\d]{24}$/i; // Expresión regular para el formato de ObjectId
+const estados = ['disponible', 'reparado', 'dañado', 'desaparecido'];
+
+export const createProductSchema = z.object({
+  unitProductId: z.string().regex(objectIdRegex, {
+    message: 'Es obligatorio ingresar el id del unitProduct',
+  }),
+  code: z
+    .string()
+    .length(12, { message: 'El codigo debe ser de 12 dígitos' })
+    .optional(),
+  estado: z
+    .enum(estados, {
+      errorMap: () => ({ message: 'Seleccione un estado' }),
+    })
+    .optional(),
+});
