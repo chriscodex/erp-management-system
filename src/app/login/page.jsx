b79f58fdd8e7d88a33559bca1e81ef444deac1c5
Christian <c.espinoza.code@gmail.com>
2024-08-20 22:19:17 -0500

    Login page initialized

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,4 +1,4 @@
-function page() {
+function LoginPage() {
   return (
     <>
       <div>Login page</div>
@@ -6,4 +6,4 @@ function page() {
   );
 }
 
-export default page
+export default LoginPage
