3f3cda6d96264774b35fdfd733b65cf7020abbe9
Christian <c.espinoza.code@gmail.com>
2024-08-21 09:49:38 -0500

    mongodb db file for conecction created

---
+++ src/libs/mongodb.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+  try {
+    const { MONGODB_URI } = process.env;
+
+    if (!MONGODB_URI) {
+      throw new Error('MONGODB_URI must be defined in .env.local');
+    }
+
+    const { connection } = await mongoose.connect(MONGODB_URI);
+    if (connection.readyState === 1) {
+      console.log('MongoDB connected');
+      return true;
+    }
+  } catch (error) {
+    console.log(error);
+    throw error
+  }
+};
