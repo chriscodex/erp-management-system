74b06ae1c823367ea2c2b8049f80871cf10c83a0
Christian <c.espinoza.code@gmail.com>
2024-08-22 18:31:22 -0500

    users data model created

--- src/models/user.js
+++ src/models/user.js
@@ -6,11 +6,28 @@ const userSchema = new Schema({
     unique: true,
     required: [true, 'DNI is required'],
   },
+  apellidos: {
+    type: String,
+    required: [true, 'Apellidos is required'],
+  },
+  nombres: {
+    type: String,
+    required: [true, 'Nombres is required'],
+  },
+  celular: {
+    type: String,
+    required: [true, 'Celular is required'],
+  },
+  dirección: {
+    type: String,
+    required: [true, 'Dirección is required'],
+  },
   password: {
     type: String,
     required: [true, 'DNI is required'],
     select: false,
   },
+  
 });
 
 const User = models.User || model('User', userSchema);
