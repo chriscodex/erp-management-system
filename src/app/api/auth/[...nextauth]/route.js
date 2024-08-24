9f24deab8268e41f429971c32ebedc847113a659
Christian <c.espinoza.code@gmail.com>
2024-08-24 08:46:50 -0500

    connectDB importe to nextauthroute

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import { connectDB } from '@/libs/mongodb'
 
 const handler = NextAuth({
   providers: [
