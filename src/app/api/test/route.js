b111e62c5ca0f4121c08e27cdd7097d20c3bae7e
Christian <c.espinoza.code@gmail.com>
2024-08-25 10:42:55 -0500

    testEndpoint now create a admin user

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,5 +1,9 @@
 import { NextResponse } from 'next/server';
+import { createUser, connectDB } from '@/models/test';
 
 export async function GET() {
+  await connectDB();
+
+  await createUser();
   return NextResponse.json({ message: 'API WORKING' });
 }
