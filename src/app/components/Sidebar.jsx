4e348eb141fdfde3ca663c8bdde98fa76b5b00b6
Christian <c.espinoza.code@gmail.com>
2024-08-29 18:44:19 -0500

    skeleton components added to sidebar user

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -6,6 +6,7 @@ import {
   RiSettings3Line,
   RiLogoutBoxRLine,
 } from '@remixicon/react';
+import Skeleton from '@mui/material/Skeleton';
 
 import { useSession, signOut } from 'next-auth/react';
 import { useState } from 'react';
@@ -71,41 +72,36 @@ function Sidebar() {
         </ul>
       </div>
       <div className="mt-auto bg-zinc-700 rounded-md mb-2 mx-2 shadow-inner select-none py-3">
-        {/* {status === 'authenticated' ? (
-          <div className="flex justify-center items-center gap-4 border-t py-4">
+        {status === 'authenticated' ? (
+          <div className="flex shadow-inner justify-center items-center gap-4 pl-3 pr-1">
             <img
               src="/fb.jpg"
               alt="user-profile"
               className="rounded-full w-[50px]"
             />
             <ul className="flex flex-col justify-center items-start">
-              <li className='font-bold'>{user}</li>
-              <li className='opacity-80'>{rol}</li>
+              <li className="font-bold">{user}</li>
+              <li className="opacity-80">{rol}</li>
             </ul>
-            <RiExpandUpDownLine className=''/>
+            <div
+              className="ml-auto cursor-pointer hover:bg-zinc-800 hover:shadow-inner rounded-full p-2"
+              onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
+            >
+              <RiExpandUpDownLine />
+            </div>
           </div>
         ) : (
-          <p className="text-sm font-bold opacity-80 pl-4 pt-4 pb-4">
-            Cargando ...{' '}
-          </p>
-        )} */}
-        <div className="flex shadow-inner justify-center items-center gap-4 pl-3 pr-1">
-          <img
-            src="/fb.jpg"
-            alt="user-profile"
-            className="rounded-full w-[50px]"
-          />
-          <ul className="flex flex-col justify-center items-start">
-            <li className="font-bold">{user}</li>
-            <li className="opacity-80">{rol}</li>
-          </ul>
-          <div
-            className="ml-auto cursor-pointer hover:bg-zinc-800 hover:shadow-inner rounded-full p-2"
-            onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
-          >
-            <RiExpandUpDownLine />
+          <div className="flex shadow-inner justify-center items-center gap-4 pl-3 pr-1">
+            <Skeleton variant="circular" width={50} height={50} />
+            <Skeleton variant="rounded" width={200} height={40} />
+            <div
+              className="ml-auto cursor-pointer hover:bg-zinc-800 hover:shadow-inner rounded-full p-2"
+              onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
+            >
+              <RiExpandUpDownLine />
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <AnimatePresence>
         {isUserMenuOpen && ( // Renderizar el menú condicionalmente
