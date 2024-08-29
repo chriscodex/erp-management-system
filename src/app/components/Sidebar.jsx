b310c17b0c9a669cb6b49ab84159a6074784f200
Christian <c.espinoza.code@gmail.com>
2024-08-29 16:29:51 -0500

    sidebar user data section implemented

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,7 +1,20 @@
 'use client';
-import { RiHome2Line } from '@remixicon/react';
+import { useSession } from 'next-auth/react';
+
+import { RiHome2Line, RiExpandUpDownLine } from '@remixicon/react';
+
+import { FormateadorNombresApellidos } from '@/utils/formateador';
 
 function Sidebar() {
+  const { data: session, status } = useSession();
+
+  const user = FormateadorNombresApellidos(
+    session?.user?.nombres,
+    session?.user?.apellidos
+  );
+
+  const rol = session?.user?.rol;
+
   const menuItems = [
     {
       icon: <RiHome2Line />,
@@ -12,15 +25,47 @@ function Sidebar() {
   return (
     <section className="w-[400px] bg-rose-700 h-screen flex flex-col">
       <div>
-        <p className='text-lg pl-4 pt-4 pb-2 opacity-65 font-light'>MENU</p>
+        <p className="text-sm font-bold opacity-80 pl-4 pt-4 pb-4">MENU</p>
         <ul>
           {menuItems.map((item) => (
-            <li key={item.label} className='flex gap-2 text-xl pl-4'>
+            <li key={item.label} className="flex gap-2 text-xl pl-4">
               {item.icon}
               <p>{item.label}</p>
             </li>
           ))}
         </ul>
+      </div>
+      <div className="w-full mt-auto">
+        {/* {status === 'authenticated' ? (
+          <div className="flex justify-center items-center gap-4 border-t py-4">
+            <img
+              src="/fb.jpg"
+              alt="user-profile"
+              className="rounded-full w-[50px]"
+            />
+            <ul className="flex flex-col justify-center items-start">
+              <li className='font-bold'>{user}</li>
+              <li className='opacity-80'>{rol}</li>
+            </ul>
+            <RiExpandUpDownLine className=''/>
+          </div>
+        ) : (
+          <p className="text-sm font-bold opacity-80 pl-4 pt-4 pb-4">
+            Cargando ...{' '}
+          </p>
+        )} */}
+        <div className="flex justify-center items-center gap-4 border-t py-4">
+          <img
+            src="/fb.jpg"
+            alt="user-profile"
+            className="rounded-full w-[50px]"
+          />
+          <ul className="flex flex-col justify-center items-start">
+            <li className="font-bold">{user}</li>
+            <li className="opacity-80">{rol}</li>
+          </ul>
+          <RiExpandUpDownLine className="" />
+        </div>
       </div>
     </section>
   );
