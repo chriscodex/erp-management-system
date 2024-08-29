ccaf7d9c24392a228ad82c1f4f919fd99f75b4ba
Christian <c.espinoza.code@gmail.com>
2024-08-29 17:35:33 -0500

    sidebar has fixed pos now

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -4,8 +4,10 @@ import { useSession } from 'next-auth/react';
 import { RiHome2Line, RiExpandUpDownLine } from '@remixicon/react';
 
 import { FormateadorNombresApellidos } from '@/utils/formateador';
+import { useState } from 'react';
 
 function Sidebar() {
+  /* Datos de la sesión */
   const { data: session, status } = useSession();
 
   const user = FormateadorNombresApellidos(
@@ -15,6 +17,10 @@ function Sidebar() {
 
   const rol = session?.user?.rol;
 
+  /* Estados para los submenus */
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+
+  /* Menu items */
   const menuItems = [
     {
       icon: <RiHome2Line />,
@@ -23,7 +29,7 @@ function Sidebar() {
   ];
 
   return (
-    <section className="w-[400px] bg-rose-700 h-screen flex flex-col">
+    <section className="fixed w-[400px] bg-rose-700 h-screen flex flex-col">
       <div>
         <p className="text-sm font-bold opacity-80 pl-4 pt-4 pb-4">MENU</p>
         <ul>
@@ -64,11 +70,23 @@ function Sidebar() {
             <li className="font-bold">{user}</li>
             <li className="opacity-80">{rol}</li>
           </ul>
-          <div className="ml-auto cursor-pointer hover:bg-rose-400 rounded-full p-2">
+          <div className="ml-auto cursor-pointer hover:bg-rose-400 rounded-full p-2" onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}>
             <RiExpandUpDownLine />
           </div>
         </div>
       </div>
+      {isUserMenuOpen && ( // Renderizar el menú condicionalmente
+        <div className="absolute left-full top-1/2 transform -translate-y-1/2 bg-white p-4 shadow-lg rounded">
+          <ul>
+            <li className="py-2 cursor-pointer hover:bg-rose-300">
+              Configuración
+            </li>
+            <li className="py-2 cursor-pointer hover:bg-rose-300">
+              Cerrar Sesión
+            </li>
+          </ul>
+        </div>
+      )}
     </section>
   );
 }
