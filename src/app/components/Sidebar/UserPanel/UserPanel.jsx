00d0d278884ffedea8871238bce545f3e73895b6
Christian <c.espinoza.code@gmail.com>
2024-08-30 10:07:51 -0500

    UserPanel variables unused removed

--- src/app/components/Sidebar/UserPanel/UserPanel.jsx
+++ src/app/components/Sidebar/UserPanel/UserPanel.jsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import { Skeleton } from '@mui/material';
+import { AnimatePresence } from 'framer-motion';
 import { RiExpandUpDownLine } from '@remixicon/react';
 
 import { UserPanelOptions } from './UserPanelOptions';
