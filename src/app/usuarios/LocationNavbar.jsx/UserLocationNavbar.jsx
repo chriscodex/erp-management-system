0e9476085db57f7c108e445653e93d89b16e9446
Christian <c.espinoza.code@gmail.com>
2024-08-31 15:03:41 -0500

    userLocationNavbar component created

--- src/app/usuarios/LocationNavbar.jsx/UserLocationNavbar.jsx
+++ src/app/usuarios/LocationNavbar.jsx/UserLocationNavbar.jsx
@@ -2,7 +2,7 @@ import { RiShieldUserFill } from '@remixicon/react';
 
 function UserLocationNavbar() {
   return (
-    <ul className='bg-component rounded-md mb-2'>
+    <ul className='bg-component rounded-md text-neutral-50'>
       <li className='flex w-full gap-3 text-xl items-center py-3 px-3'>
         <RiShieldUserFill />
         <span>Usuarios</span>
