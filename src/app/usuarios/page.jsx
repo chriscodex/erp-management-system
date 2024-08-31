603bc641ed62881123703853cf36d0b8c27251cb
Christian <c.espinoza.code@gmail.com>
2024-08-31 15:03:26 -0500

    h1 and userLocationNavbar added

--- src/app/usuarios/page.jsx
+++ src/app/usuarios/page.jsx
@@ -1,9 +1,13 @@
+import { UserLocationNavbar } from '@/app/usuarios/LocationNavbar.jsx/UserLocationNavbar';
+
 function UsuariosPage() {
   return (
     <>
-      <div>usuarios</div>
+      <UserLocationNavbar />
+      <h1 className='text-3xl text-component font-bold ml-2 my-2'>Todos los Usuarios</h1>
+      
     </>
   );
 }
 
-export default UsuariosPage
+export default UsuariosPage;
