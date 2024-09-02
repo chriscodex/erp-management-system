60f103bf5795bbb504dad6fa0ed4d5ff8e7de8f1
Christian <c.espinoza.code@gmail.com>
2024-09-02 17:43:54 -0500

    columns for userTable defined

---
+++ src/app/usuarios/columns.jsx
@@ -0,0 +1,20 @@
+"use client"
+
+export const columns = [
+  {
+    accessorKey: "dni",
+    header: "Status",
+  },
+  {
+    accessorKey: "nombres",
+    header: "Nombres",
+  },
+  {
+    accessorKey: "apellidos",
+    header: "Apellidos",
+  },
+  {
+    accessorKey: "celular",
+    header: "Celular",
+  },
+]
