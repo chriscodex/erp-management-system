53ebe3ee9b726cbaae41f1f576286a17a4ea4a36
Christian <c.espinoza.code@gmail.com>
2024-09-03 18:14:48 -0500

    columns formated added

--- src/app/usuarios/columns.jsx
+++ src/app/usuarios/columns.jsx
@@ -1,20 +1,32 @@
-"use client"
+'use client';
 
 export const columns = [
   {
-    accessorKey: "dni",
-    header: "Status",
+    accessorKey: 'dni',
+    header: 'Status',
+    header: () => <div className="text-right">DNI</div>,
+    cell: ({ row }) => {
+      const digits = row.getValue('dni');
+      const formattedNumber = digits.replace(/\D/g, '');
+      const formated = formattedNumber.substring(0, 8)
+
+      return (
+        <div className="text-right font-medium">
+          {formated}
+        </div>
+      );
+    },
   },
   {
-    accessorKey: "nombres",
-    header: "Nombres",
+    accessorKey: 'nombres',
+    header: 'Nombres',
   },
   {
-    accessorKey: "apellidos",
-    header: "Apellidos",
+    accessorKey: 'apellidos',
+    header: 'Apellidos',
   },
   {
-    accessorKey: "celular",
-    header: "Celular",
+    accessorKey: 'celular',
+    header: 'Celular',
   },
-]
+];
