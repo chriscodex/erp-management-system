99c2d5bc99aa6c81a1986b2f3b67ae78ff116475
Christian <c.espinoza.code@gmail.com>
2024-09-04 07:09:15 -0500

    fetch data y getdata function implemented in apiClient

---
+++ src/app/usuarios/Infraestructura/apiClient.js
@@ -0,0 +1,36 @@
+import { getAllUsersUrl } from './apiUrls';
+
+async function fetchData(url) {
+  try {
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function delay(ms) {
+  try {
+    return new Promise((resolve) => setTimeout(resolve, ms)); // eslint-disable-line
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getAllUsers() {
+  try {
+    return fetchData(getAllUsersUrl);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { fetchData, delay, getAllUsers };
