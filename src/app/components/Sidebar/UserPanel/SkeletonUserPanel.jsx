982631df49b20f5348c68118d47fcd85b87152ed
Christian <c.espinoza.code@gmail.com>
2024-09-04 07:24:32 -0500

    custom styles added to skeleton user panel

--- src/app/components/Sidebar/UserPanel/SkeletonUserPanel.jsx
+++ src/app/components/Sidebar/UserPanel/SkeletonUserPanel.jsx
@@ -1,20 +1,12 @@
-'use client';
 import { Skeleton } from '@mui/material';
 import { RiExpandUpDownLine } from '@remixicon/react';
-import { motion } from 'framer-motion';
 
-function SkeletonUserPanel({
-  isUserPanelOptionsOpen,
-  setIsUserPanelOptionsOpen,
-}) {
+function SkeletonUserPanel() {
   return (
     <>
       <Skeleton variant="circular" width={50} height={50} />
       <Skeleton variant="rounded" width={200} height={40} />
-      <div
-        className="ml-auto cursor-pointer hover:bg-zinc-800 hover:shadow-inner rounded-full p-2"
-        onClick={() => setIsUserPanelOptionsOpen(!isUserPanelOptionsOpen)}
-      >
+      <div className="ml-auto cursor-pointer hover:bg-zinc-800 hover:shadow-inner rounded-full p-2">
         <RiExpandUpDownLine />
       </div>
     </>
