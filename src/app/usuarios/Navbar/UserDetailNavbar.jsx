0e3c2d2d1efb0f8b02fae63949beeaa86d21cbf1
Christian <c.espinoza.code@gmail.com>
2024-09-03 22:26:08 -0500

    userDetailnavbar created

---
+++ src/app/usuarios/Navbar/UserDetailNavbar.jsx
@@ -0,0 +1,15 @@
+import { RiShieldUserFill, RiArrowRightSLine } from '@remixicon/react';
+
+function UserNavbar() {
+  return (
+    <ul className="bg-component rounded-md text-neutral-50">
+      <li className="flex w-full gap-3 text-xl items-center py-3 px-3">
+        <RiShieldUserFill />
+        <span>Usuarios</span>
+        <RiArrowRightSLine />
+      </li>
+    </ul>
+  );
+}
+
+export { UserNavbar };
