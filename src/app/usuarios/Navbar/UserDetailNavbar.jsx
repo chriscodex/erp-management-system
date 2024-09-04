57d4ffb0a6e7b73d50431326a9ca3f2c607daa3e
Christian <c.espinoza.code@gmail.com>
2024-09-03 22:35:50 -0500

    userDetailNavbar now uses the path url

--- src/app/usuarios/Navbar/UserDetailNavbar.jsx
+++ src/app/usuarios/Navbar/UserDetailNavbar.jsx
@@ -1,15 +1,16 @@
 import { RiShieldUserFill, RiArrowRightSLine } from '@remixicon/react';
 
-function UserNavbar() {
+function UserDetailNavbar({ userDni }) {
   return (
     <ul className="bg-component rounded-md text-neutral-50">
       <li className="flex w-full gap-3 text-xl items-center py-3 px-3">
         <RiShieldUserFill />
         <span>Usuarios</span>
         <RiArrowRightSLine />
+        <span>{userDni}</span>
       </li>
     </ul>
   );
 }
 
-export { UserNavbar };
+export { UserDetailNavbar };
