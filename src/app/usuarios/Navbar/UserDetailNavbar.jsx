4bf30952dcf9da6b3f4cd57ead249936f5f13657
Christian <c.espinoza.code@gmail.com>
2024-09-06 16:51:32 -0500

    userDetailNavbar styles added

--- src/app/usuarios/Navbar/UserDetailNavbar.jsx
+++ src/app/usuarios/Navbar/UserDetailNavbar.jsx
@@ -2,7 +2,7 @@ import { RiShieldUserFill, RiArrowRightSLine } from '@remixicon/react';
 
 function UserDetailNavbar({ userDni }) {
   return (
-    <ul className="bg-component rounded-md text-neutral-50">
+    <ul className="bg-component rounded-md text-neutral-50 w-full max-w-7xl mr-auto">
       <li className="flex w-full gap-3 text-xl items-center py-3 px-3">
         <RiShieldUserFill />
         <span>Usuarios</span>
