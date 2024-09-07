45934de98532eea0d0d81b0e7043145cd711bfb9
Christian <c.espinoza.code@gmail.com>
2024-09-07 18:03:36 -0500

    Layout for usuarios fixed

--- src/app/usuarios/layout.jsx
+++ src/app/usuarios/layout.jsx
@@ -1,12 +1,10 @@
 import { Sidebar } from '@/app/components/Sidebar/Sidebar.jsx';
-
+ 
 export default function Layout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <Sidebar />
-        <div className="ml-[350px] p-2">{children}</div>
-      </body>
-    </html>
+    <div>
+      <Sidebar />
+      <div className="ml-[350px] p-2">{children}</div>
+    </div>
   );
 }
