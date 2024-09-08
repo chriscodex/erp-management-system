53412ed54f2af318f577d06ced9561530d649232
Christian <c.espinoza.code@gmail.com>
2024-09-07 22:44:23 -0500

    eslint disable line added

--- src/hooks/use-toast.js
+++ src/hooks/use-toast.js
@@ -5,13 +5,6 @@ import * as React from "react"
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
 
-const actionTypes = {
-  ADD_TOAST: "ADD_TOAST",
-  UPDATE_TOAST: "UPDATE_TOAST",
-  DISMISS_TOAST: "DISMISS_TOAST",
-  REMOVE_TOAST: "REMOVE_TOAST"
-}
-
 let count = 0
 
 function genId() {
@@ -19,7 +12,7 @@ function genId() {
   return count.toString();
 }
 
-const toastTimeouts = new Map()
+const toastTimeouts = new Map() // eslint-disable-line
 
 const addToRemoveQueue = (toastId) => {
   if (toastTimeouts.has(toastId)) {
