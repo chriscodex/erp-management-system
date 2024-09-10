917ad93f707d0732cebdf8bf781328c860091366
Christian <c.espinoza.code@gmail.com>
2024-09-10 11:39:19 -0500

    nuevo page breadcrumb implemented

--- src/app/usuarios/nuevo/page.jsx
+++ src/app/usuarios/nuevo/page.jsx
@@ -1,225 +1,55 @@
-'use client';
+import Link from 'next/link';
 
-import { useState } from 'react';
-
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { AppSidebar } from '@/components/app-sidebar';
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import { Switch } from '@/components/ui/switch';
-import { useToast } from '@/hooks/use-toast';
-import {
-  Loader2,
-  User,
-  Lock,
-  Shield,
-  MapPin,
-  CreditCard,
-  Phone,
-} from 'lucide-react';
+  SidebarInset,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
 
-import { NewUserNavbar } from '../Navbar/NewUserNavbar';
+import { FormNewUser } from './FormNewUser';
 
-const initialFormData = {
-  dni: '',
-  nombres: '',
-  celular: '',
-  dirección: '',
-  rol: 'Vendedor',
-  password: '',
-  isActive: true,
-};
-
-export default function CreateUser() {
-  const { toast } = useToast();
-
-  const [formData, setFormData] = useState(initialFormData);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const handleRoleChange = (value) => {
-    setFormData((prev) => ({ ...prev, rol: value }));
-  };
-
-  const handleActiveChange = (checked) => {
-    setFormData((prev) => ({ ...prev, isActive: checked }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-
-    // Simular una llamada a la API
-    await new Promise((resolve) => setTimeout(resolve, 1500)); //eslint-disable-line
-
-    // Simular una respuesta exitosa
-    setIsLoading(false);
-    toast(
-      {
-        title: 'Usuario creado',
-        description: `Se ha creado el usuario ${formData.name} exitosamente.`,
-      },
-      { duration: 100 }
-    );
-
-    // Resetear el formulario
-    setFormData(initialFormData);
-  };
-
+export default function Page() {
   return (
     <>
-      <NewUserNavbar />
-      <div className="container mx-auto py-4">
-        <Card className="w-full max-w-7xl mr-auto">
-          <CardHeader>
-            <CardTitle className="text-2xl">Crear Nuevo Usuario</CardTitle>
-            <CardDescription>
-              Ingrese los detalles del nuevo usuario para el sistema.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div className="space-y-2">
-                <Label htmlFor="dni">DNI</Label>
-                <div className="relative">
-                  <CreditCard className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                  <Input
-                    id="dni"
-                    name="dni"
-                    type="text"
-                    placeholder="12345678"
-                    value={formData.dni}
-                    onChange={handleInputChange}
-                    className="pl-8"
-                    required
-                    autocomplete="off"
-                  />
-                </div>
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="nombres">Nombre Completo</Label>
-                <div className="relative">
-                  <User className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                  <Input
-                    id="nombres"
-                    name="nombres"
-                    placeholder="John Doe"
-                    value={formData.nombres}
-                    onChange={handleInputChange}
-                    className="pl-8"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="celular">Celular</Label>
-                <div className="relative">
-                  <Phone className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                  <Input
-                    id="celular"
-                    name="celular"
-                    type="number"
-                    value={formData.celular}
-                    onChange={handleInputChange}
-                    className="pl-8"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="direccion">Dirección</Label>
-                <div className="relative">
-                  <MapPin className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                  <Input
-                    id="direccion"
-                    name="direccion"
-                    type="direccion"
-                    value={formData.dirección}
-                    onChange={handleInputChange}
-                    className="pl-8"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="rol">Rol</Label>
-                <div className="relative">
-                  <Shield className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                  <Select
-                    onValueChange={handleRoleChange}
-                    value={formData.rol}
-                    required
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 h-4 dark:bg-white bg-muted-foreground"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <Link
+                    href="/usuarios"
+                    className="text-muted-foreground transition-colors hover:text-foreground"
                   >
-                    <SelectTrigger className="w-full pl-8">
-                      <SelectValue placeholder="Seleccione un rol" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="Vendedor">Vendedor</SelectItem>
-                      <SelectItem value="Administrador">
-                        Administrador
-                      </SelectItem>
-                      <SelectItem value="Tecnico">Técnico</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="password">Contraseña</Label>
-                <div className="relative">
-                  <Lock className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                  <Input
-                    id="password"
-                    name="password"
-                    type="password"
-                    placeholder="••••••••"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    className="pl-8"
-                    required
-                    minLength={8}
-                  />
-                </div>
-              </div>
-
-              <div className="flex items-center space-x-2">
-                <Switch
-                  id="isActive"
-                  checked={formData.isActive}
-                  onCheckedChange={handleActiveChange}
-                />
-                <Label htmlFor="isActive">Usuario Activo</Label>
-              </div>
-              <Button className="w-full" disabled={isLoading}>
-                {isLoading ? (
-                  <>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                    Creando Usuario...
-                  </>
-                ) : (
-                  'Crear Usuario'
-                )}
-              </Button>
-            </form>
-          </CardContent>
-        </Card>
-      </div>
+                    Usuarios
+                  </Link>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Nuevo Usuario</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <FormNewUser />
+        </div>
+      </SidebarInset>
     </>
   );
 }
