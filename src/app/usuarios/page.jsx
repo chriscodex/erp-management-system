822559e135d8504804a97a0ab8ba94eebb183fed
Christian <c.espinoza.code@gmail.com>
2024-09-10 12:01:10 -0500

    usuariosTable now recieve data

--- src/app/usuarios/page.jsx
+++ src/app/usuarios/page.jsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 
-import { AppSidebar } from '@/components/app-sidebar';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -19,10 +18,20 @@ import { columns } from '@/app/usuarios/UsersTable//columns';
 
 import { getAllUsers } from '@/app/usuarios/Infraestructura/apiClient';
 
-export default function Page() {
+export default async function Page() {
+  const data = await getAllUsers();
+
+  const dataFormated = data.map((user) => {
+    const fullName = {
+      fullName: user.nombres + ' ' + user.apellidos,
+      ...user,
+    };
+    delete fullName.apellidos;
+    delete fullName.nombres;
+    return fullName;
+  });
   return (
     <>
-      <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
@@ -45,7 +54,7 @@ export default function Page() {
             <Button>Agregar Nuevo Usuario</Button>
           </Link>
 
-          <DataTable columns={columns} data={getAllUsers()} />
+          <DataTable columns={columns} data={dataFormated} />
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
