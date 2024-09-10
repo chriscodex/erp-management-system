01b4e772923e0850d1b4f9bad7053ca7e6c2c658
Christian <c.espinoza.code@gmail.com>
2024-09-10 11:40:00 -0500

    sidebar provider implemented in session provider

--- src/components/providers/Providers.jsx
+++ src/components/providers/Providers.jsx
@@ -1,13 +1,15 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
+
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
 function Providers({ children }) {
   return (
     <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="dark">
-        {children}
+        <SidebarProvider>{children}</SidebarProvider>
       </ThemeProvider>
       <Toaster />
     </SessionProvider>
