cbaf128a0668e610a48440cd9d225559bbda57d5
Christian <c.espinoza.code@gmail.com>
2024-09-11 12:09:11 -0500

    note results for userTable changed

--- src/app/usuarios/UsersTable/data-table.jsx
+++ src/app/usuarios/UsersTable/data-table.jsx
@@ -71,7 +71,7 @@ export function DataTable({ columns, data }) {
                   colSpan={columns.length}
                   className="h-24 text-center"
                 >
-                  No results.
+                  Sin resultados.
                 </TableCell>
               </TableRow>
             )}
