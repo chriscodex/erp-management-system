61b822b323bbcf8cb0a362b5d7644e15dbcad3d0
Christian <c.espinoza.code@gmail.com>
2024-09-14 16:47:46 -0500

    prod todos page initial structure created

---
+++ src/app/inventario/productos/todos/page.jsx
@@ -0,0 +1,11 @@
+import { Label } from '@/components/ui/label';
+
+function ProductosTodosPage() {
+  return (
+    <>
+      <Label>ProductosTodos</Label>
+    </>
+  );
+}
+
+export default ProductosTodosPage
