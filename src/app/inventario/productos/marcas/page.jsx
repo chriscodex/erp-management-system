1c0b3660319776eba3cef3aa89028f2844b60333
Christian <c.espinoza.code@gmail.com>
2024-09-14 16:47:59 -0500

    prod marcas page initial structure created

---
+++ src/app/inventario/productos/marcas/page.jsx
@@ -0,0 +1,11 @@
+import { Label } from '@/components/ui/label';
+
+function ProductosMarcasPage() {
+  return (
+    <>
+      <Label>ProductosMarcas</Label>
+    </>
+  );
+}
+
+export default ProductosMarcasPage
