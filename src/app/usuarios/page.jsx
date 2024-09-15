57a594a9152702003872fcac59962637f5c6bf27
Christian <c.espinoza.code@gmail.com>
2024-09-15 00:15:45 -0500

    Usuarios title added in usuarios page

--- src/app/usuarios/page.jsx
+++ src/app/usuarios/page.jsx
@@ -4,6 +4,8 @@ import { DataTable } from '@/app/usuarios/UsersTable/data-table';
 import { columns } from '@/app/usuarios/UsersTable/columns';
 import { NavbarSimple } from '@/components/navbar/NavbarSimple';
 import { Button } from '@/components/ui/button';
+import { Label } from '@radix-ui/react-label';
+import { User2Icon } from 'lucide-react';
 
 import { getAllUsers } from '@/app/usuarios/Infraestructura/apiClient';
 
@@ -23,9 +25,15 @@ export default async function Page() {
   return (
     <>
       <NavbarSimple title="Usuarios">
-        <Link href="/usuarios/nuevo" className="flex justify-end">
-          <Button>Agregar Nuevo Usuario</Button>
-        </Link>
+        <div className="flex justify-between">
+          <div className="flex items-center gap-2">
+            <User2Icon className="h-9 w-9" />
+            <Label className="text-4xl font-bold">Usuarios</Label>
+          </div>
+          <Link href="/usuarios/nuevo" className="flex justify-end">
+            <Button>Agregar Nuevo Usuario</Button>
+          </Link>
+        </div>
         <DataTable columns={columns} data={dataFormated} />
       </NavbarSimple>
     </>
