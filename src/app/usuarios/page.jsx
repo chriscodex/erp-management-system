29909178b702129bc82eea2cc96e1408d4592f57
Christian <c.espinoza.code@gmail.com>
2024-09-14 20:08:07 -0500

    usuarios page now uses usuarios

--- src/app/usuarios/page.jsx
+++ src/app/usuarios/page.jsx
@@ -1,20 +1,9 @@
 import Link from 'next/link';
 
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-} from '@/components/ui/breadcrumb';
-import { Separator } from '@/components/ui/separator';
-import {
-  SidebarInset,
-  SidebarTrigger,
-} from '@/components/ui/sidebar';
-import { Button } from '@/components/ui/button';
-
 import { DataTable } from '@/app/usuarios/UsersTable/data-table';
-import { columns } from '@/app/usuarios/UsersTable//columns';
+import { columns } from '@/app/usuarios/UsersTable/columns';
+import { NavbarSimple } from '@/components/navbar/NavbarSimple';
+import { Button } from '@/components/ui/button';
 
 import { getAllUsers } from '@/app/usuarios/Infraestructura/apiClient';
 
@@ -32,37 +21,12 @@ export default async function Page() {
   });
   return (
     <>
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 h-4 dark:bg-white bg-muted-foreground"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Usuarios</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <Link href="/usuarios/nuevo" className='flex justify-end'>
-            <Button>Agregar Nuevo Usuario</Button>
-          </Link>
-
-          <DataTable columns={columns} data={[]} />
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div>
-      </SidebarInset>
+      <NavbarSimple title="Usuarios">
+        <Link href="/usuarios/nuevo" className="flex justify-end">
+          <Button>Agregar Nuevo Usuario</Button>
+        </Link>
+        <DataTable columns={columns} data={dataFormated} />
+      </NavbarSimple>
     </>
   );
 }
