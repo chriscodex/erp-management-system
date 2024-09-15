a923461468058db72f30931eb7d8e73f4ecf75ae
Christian <c.espinoza.code@gmail.com>
2024-09-15 08:07:11 -0500

    test route now poblate users collection

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,9 +1,12 @@
 import { NextResponse } from 'next/server';
 import { createUser, connectDB } from '@/models/test';
+import { seedUsers } from '@/db/seed';
 
 export async function GET() {
   await connectDB();
 
-  await createUser();
+  await seedUsers();
+
+  // await createUser();
   return NextResponse.json({ message: 'API WORKING' });
 }
