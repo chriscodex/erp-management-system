8f47a56d25cb70b85e050106192208592d654c96
Christian <c.espinoza.code@gmail.com>
2024-09-14 20:08:48 -0500

    inventario-productos-todos page nos uses navbar dynamic

--- src/app/inventario/productos/todos/page.jsx
+++ src/app/inventario/productos/todos/page.jsx
@@ -1,11 +1,31 @@
 import { Label } from '@/components/ui/label';
+import { NavbarDynamic } from '@/components/navbar/NavbarDynamic';
 
 function ProductosTodosPage() {
+  const titles = [
+    {
+      title: 'Inventario',
+      href: '/inventario/todos',
+      active: false,
+    },
+    {
+      title: 'Productos',
+      href: '/inventario/productos/todos',
+      active: false,
+    },
+    {
+      title: 'Todos los productos',
+      href: '/inventario/productos/todos',
+      active: false,
+    },
+  ]
   return (
     <>
-      <Label>ProductosTodos</Label>
+      <NavbarDynamic titles={titles}>
+        <Label>ProductosTodos</Label>
+      </NavbarDynamic>
     </>
   );
 }
 
-export default ProductosTodosPage
+export default ProductosTodosPage;
