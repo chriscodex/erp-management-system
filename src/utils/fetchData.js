beeae33e1e1e5fd83771cdd4f1d48ca9006506a9
Christian <c.espinoza.code@gmail.com>
2024-09-16 12:12:50 -0500

    getDataByDni function created

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import https from 'https';
+
 async function delay(ms) {
   try {
     return new Promise((resolve) => setTimeout(resolve, ms)); // eslint-disable-line
@@ -23,4 +26,47 @@ async function getData(url) {
   }
 }
 
-export { delay, getData };
+async function getDataByDni(dni) {
+  try {
+    // Datos
+    const token = process.env.NEXT_TOKEN_RENIEC;
+
+    // URL de la API
+    const apiUrl = `${process.env.NEXT_RENIEC_URL}/dni?numero=${dni}`;
+
+    // Configuración de la solicitud
+    const axiosConfig = {
+      method: 'get',
+      url: apiUrl,
+      headers: {
+        Referer: 'https://apis.net.pe/consulta-dni-api',
+        Authorization: `Bearer ${token}`,
+      },
+      // Utiliza el módulo 'https' para crear el agente httpsAgent
+      httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+    };
+
+    // Realizar la llamada a la API
+    const response = await axios(axiosConfig);
+
+    return response.data;
+
+    // const response = {
+    //   data: {
+    //     nombres: 'CHRISTIAN GONZALO',
+    //     apellidoPaterno: 'ESPINOZA',
+    //     apellidoMaterno: 'CADILLO',
+    //     tipoDocumento: '1',
+    //     numeroDocumento: '74062106',
+    //   },
+    // };
+
+    // setTimeout(() => {
+    //   res.json(response.data);
+    // }, 1000);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { delay, getDataByDni, getData };
