90c90887b18433281030170f7031f539ccc3aa7e
Christian <c.espinoza.code@gmail.com>
2024-09-16 12:26:47 -0500

    getDataByRuc function implemented and tested correctly

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,12 +1,28 @@
 import { NextResponse } from 'next/server';
 import { createUser, connectDB } from '@/models/test';
 import { seedUsers } from '@/db/seed';
+import { getDataByDni } from '@/utils/fetchData';
+import { getDataByRuc } from '@/utils/fetchData';
 
 export async function GET() {
-  await connectDB();
+  try {
+    await connectDB();
 
-  await seedUsers();
+    const dni = await getDataByDni('74062106');
 
-  // await createUser();
-  return NextResponse.json({ message: 'API WORKING' });
+    const ruc = await getDataByRuc('20428729201');
+
+    console.log(ruc);
+
+    // await seedUsers();
+
+    // await createUser();
+    return NextResponse.json(ruc);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
 }
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -29,10 +29,10 @@ async function getData(url) {
 async function getDataByDni(dni) {
   try {
     // Datos
-    const token = process.env.NEXT_TOKEN_RENIEC;
+    const token = process.env.TOKEN_API_RUC_DNI;
 
     // URL de la API
-    const apiUrl = `${process.env.NEXT_RENIEC_URL}/dni?numero=${dni}`;
+    const apiUrl = `${process.env.NEXT_API_RUC_DNI_URL}/reniec/dni?numero=${dni}`;
 
     // Configuración de la solicitud
     const axiosConfig = {
@@ -69,4 +69,60 @@ async function getDataByDni(dni) {
   }
 }
 
-export { delay, getDataByDni, getData };
+async function getDataByRuc(ruc) {
+  try {
+    // Datos
+    const token = process.env.TOKEN_API_RUC_DNI;
+    // URL de la API
+    const apiUrl = `${process.env.NEXT_API_RUC_DNI_URL}/sunat/ruc?numero=${ruc}`;
+    // Configuración de la solicitud
+    const axiosConfig = {
+      method: 'get',
+      url: apiUrl,
+      headers: {
+        Referer: 'http://apis.net.pe/api-ruc',
+        Authorization: `Bearer ${token}`,
+      },
+      // Utiliza el módulo 'https' para crear el agente httpsAgent
+      httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+    };
+    // Realizar la llamada a la API
+    const response = await axios(axiosConfig);
+
+    return response.data;
+
+    // const response = {
+    //   data: {
+    //     razonSocial: 'ARENERA SAN MARTIN DE PORRAS S.A.',
+    //     tipoDocumento: '6',
+    //     numeroDocumento: '20428729201',
+    //     estado: 'ACTIVO',
+    //     condicion: 'HABIDO',
+    //     direccion: 'AV. MONTEVERDE NRO 197 ',
+    //     ubigeo: '150103',
+    //     viaTipo: 'AV.',
+    //     viaNombre: 'MONTEVERDE',
+    //     zonaCodigo: '-',
+    //     zonaTipo: '-',
+    //     numero: '197',
+    //     interior: '-',
+    //     lote: '-',
+    //     dpto: '-',
+    //     manzana: '-',
+    //     kilometro: '-',
+    //     distrito: 'ATE',
+    //     provincia: 'LIMA',
+    //     departamento: 'LIMA',
+    //     EsAgenteRetencion: false,
+    //   },
+    // };
+
+    // setTimeout(() => {
+    //   res.json(response.data);
+    // }, 500);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { delay, getData, getDataByDni, getDataByRuc };
