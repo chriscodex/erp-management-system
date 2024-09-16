b93f74f1b89a1065f89ff95cf35cba71e73a5933
Christian <c.espinoza.code@gmail.com>
2024-09-16 09:50:11 -0500

    skeleton implemented for sidebar user data

--- src/components/nav-user.jsx
+++ src/components/nav-user.jsx
@@ -22,6 +22,7 @@ import {
 } from '@/components/ui/sidebar';
 
 import { FormateadorNombresApellidos } from '@/utils/formateador';
+import { Skeleton } from './ui/skeleton';
 
 export function NavUser() {
   const { isMobile } = useSidebar();
@@ -56,14 +57,8 @@ export function NavUser() {
                 size="lg"
                 className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
               >
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src="/avatars/avatar-default.jpg" alt="avatar" />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{fullName}</span>
-                  <span className="truncate text-xs">{user?.rol}</span>
-                </div>
+                <Skeleton className="h-8 w-8 rounded-lg" />
+                <Skeleton  className="h-full flex-1 max-w-[--skeleton-width]" />
                 <ChevronsUpDown className="ml-auto size-4" />
               </SidebarMenuButton>
             )}
