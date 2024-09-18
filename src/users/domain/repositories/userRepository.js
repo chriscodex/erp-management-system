f6a2d17964594f414e34afa588ebc96a2a151773
Christian <c.espinoza.code@gmail.com>
2024-09-17 20:10:54 -0500

    userRepository created

---
+++ src/users/domain/repositories/userRepository.js
@@ -0,0 +1,32 @@
+import bcryptjs from 'bcryptjs';
+
+import { User } from '@/models/user';
+
+const userRepository = {
+  async createUser(user) {
+    try {
+      const { password } = user;
+      const passwordHash = await bcryptjs.hash(password, 12);
+
+      const newUser = new User({
+        ...user,
+        password: passwordHash,
+      });
+
+      const savedUser = await newUser.save();
+      return savedUser;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async getAllUsers() {
+    try {
+      const users = await User.find();
+      return users;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+
+export { userRepository };
