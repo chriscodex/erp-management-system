d3a5addd1e603c2644e464580399d9f948866f3b
Christian <c.espinoza.code@gmail.com>
2024-09-19 12:51:36 -0500

    test route refactorized

---
+++ src/app/api/test/[type]/route.js
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import { getDataByDniTest, getDataByRucTest, seedTest } from '@/lib/pruebas';
+
+export async function GET(request, { params }) {
+  try {
+    switch (params.type) {
+      case 'seed':
+        const seed = await seedTest();
+        return NextResponse.json(seed);
+
+      case 'dni':
+        const dni = await getDataByDniTest();
+        return NextResponse.json(dni);
+
+      case 'ruc':
+        const ruc = await getDataByRucTest();
+        return NextResponse.json(ruc);
+
+      default:
+        return NextResponse.json({ error: 'Invalid type' }, { status: 400 });
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
+}
