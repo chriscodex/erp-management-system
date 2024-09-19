94879e97785de341d720ca238e7871b04337b307
Christian <c.espinoza.code@gmail.com>
2024-09-19 12:38:15 -0500

    rucapi test created

--- src/lib/pruebas.js
+++ src/lib/pruebas.js
@@ -1,7 +1,34 @@
-import { getDataByDni } from './fetchData';
+import { getDataByDni, getDataByRuc } from './fetchData';
+import { seed } from '@/db/seed';
+import { connectDB } from '@/db/mongodb';
+
+export async function seedTest() {
+  try {
+    await connectDB();
+    await seed();
+    return 'Seed test executed successfully';
+  } catch (error) {
+    console.log('Seed test error:', error);
+  }
+}
 
 export async function getDataByDniTest() {
-  const dni = await getDataByDni('74062106');
+  try {
+    const dni = await getDataByDni('74062106');
 
-  return dni;
+    return dni;
+  } catch (error) {
+    console.log('getDataByDniTest error:', error);
+  }
+}
+
+
+export async function getDataByRucTest() {
+  try {
+    const ruc = await getDataByRuc('20428729201');
+
+    return ruc;
+  } catch (error) {
+    console.log('getDataByRucTest error:', error);
+  }
 }
