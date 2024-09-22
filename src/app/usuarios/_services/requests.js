fc83275e11ec5b1d0fcac7835c81230f3372eb88
Christian <c.espinoza.code@gmail.com>
2024-09-22 00:38:27 -0500

    comments added to getDni function

--- src/app/usuarios/_services/requests.js
+++ src/app/usuarios/_services/requests.js
@@ -12,6 +12,12 @@ export async function getAllUsers() {
   }
 }
 
+/**
+ * Obtiene datos de una persona por su DNI, formatea los nombres y apellidos, y devuelve el resultado.
+ * @param {string} dni - El número de DNI de la persona. Por defecto, es una cadena vacía.
+ * @returns {Object} - Un objeto con los nombres y apellidos formateados en mayúscula y minúscula.
+ * La función realiza una solicitud a una API externa para obtener la información personal correspondiente al DNI proporcionado, luego aplica formato a los nombres y apellidos para que estén correctamente capitalizados (primera letra en mayúscula, el resto en minúscula).
+ */
 export async function getDataByDni(dni = '') {
   try {
     const { nombres, apellidoPaterno, apellidoMaterno } = await getData(
