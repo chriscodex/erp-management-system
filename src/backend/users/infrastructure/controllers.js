99f9d0000901a4dbfd98a5c593826c9ad7696b18
Christian <c.espinoza.code@gmail.com>
2024-09-24 15:48:00 -0500

    getAllUserscontroller created

--- src/backend/users/infrastructure/controllers.js
+++ src/backend/users/infrastructure/controllers.js
@@ -1,16 +1,29 @@
-import { createUser } from '@/backend/users/application/users.service';
+import { UsersService } from '@/backend/users/application/users.service';
 import { connectDB } from '@/db/mongodb';
+
+const userService = new UsersService();
 
 export async function createUserController(user) {
   try {
     await connectDB();
 
     /* Responses { payload, status} */
-    const createdUser = await createUser(user);
+    const createdUser = await userService.createUser(user);
 
     return createdUser;
   } catch (error) {
     console.error('Error creating user:', error);
     throw new Error('Internal Server Error - createUserController');
+  }
+}
+
+export async function getAllUsersController() {
+  try {
+    await connectDB();
+    const users = await userService.getAllUsers();
+    return users;
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw new Error('Internal Server Error - getAllUsersController');
   }
 }
