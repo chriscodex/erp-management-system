e94bc888bec93776fb68c00de9beab837bc53444
Christian <c.espinoza.code@gmail.com>
2024-09-25 18:47:50 -0500

    external api doesnt response status code in payload

--- src/app/api/external/dni/route.js
+++ src/app/api/external/dni/route.js
@@ -4,9 +4,14 @@ import { getUserDataByDniController } from '@/backend/searchedUsers/infrastructu
 export async function GET(request) {
   try {
     const dni = request.nextUrl.searchParams.get('number');
-    
-    const userData = await getUserDataByDniController(dni);
-    return NextResponse.json(userData);
+
+    const { payload, status } = await getUserDataByDniController(dni);
+
+    if (status !== 200) {
+      return NextResponse.json({ error: payload }, { status: 400 });
+    }
+
+    return NextResponse.json({ payload }, { status });
   } catch (error) {
     return NextResponse.json(
       { error: 'Internal Server Error' },
