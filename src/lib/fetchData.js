8408b62ef5a506d2980c4fade575455ecf8943c2
Christian <c.espinoza.code@gmail.com>
2024-09-26 10:06:28 -0500

    delay removed from fetch data folder

--- src/lib/fetchData.js
+++ src/lib/fetchData.js
@@ -1,16 +1,23 @@
 import axios from 'axios';
 
-export async function delay(ms) {
-  try {
-    return new Promise((resolve) => setTimeout(resolve, ms)); // eslint-disable-line
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 export async function fetchData(url) {
   try {
     const response = await axios.get(url, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+export async function postData(url, data) {
+  try {
+    const response = await axios.post(url, data, {
       headers: {
         'Content-Type': 'application/json',
       },
