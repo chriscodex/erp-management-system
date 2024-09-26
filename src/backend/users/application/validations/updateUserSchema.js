9bd11fe8e9edbde8ba41a34d5f3daa6201d1c714
Christian <c.espinoza.code@gmail.com>
2024-09-26 08:44:42 -0500

    updateUserSchema created

---
+++ src/backend/users/application/validations/updateUserSchema.js
@@ -0,0 +1,38 @@
+import { z } from 'zod';
+
+const roles = ['Administrador', 'Vendedor', 'Tecnico'];
+
+export const updateUserSchema = z.object({
+  dni: z.string().length(8, 'El DNI debe tener 8 dígitos').optional(), // El DNI puede ser opcional
+  apellidos: z
+    .string()
+    .min(3, {
+      message: 'Los apellidos deben tener al menos 3 caracteres',
+    })
+    .max(50, {
+      message: 'Los apellidos no pueden tener más de 50 caracteres',
+    })
+    .optional(), // Los apellidos pueden ser opcionales
+  nombres: z
+    .string()
+    .min(3, {
+      message: 'Los nombres deben tener al menos 3 caracteres',
+    })
+    .max(50, {
+      message: 'Los nombres no pueden tener más de 50 caracteres',
+    })
+    .optional(), // Los nombres pueden ser opcionales
+  celular: z
+    .string()
+    .min(4, { message: 'El celular debe tener al menos 4 dígitos' })
+    .optional(), // El celular puede ser opcional
+  direccion: z.string().min(3, {
+    message: 'La dirección debe tener al menos 3 caracteres',
+  }).optional(), // La dirección puede ser opcional
+  rol: z.enum(roles, {
+    errorMap: () => ({ message: 'Seleccione un rol' }),
+  }).optional(), // El rol puede ser opcional
+  password: z.string().min(3, {
+    message: 'La contraseña debe tener al menos 3 caracteres',
+  }).optional(), // La contraseña puede ser opcional
+});
