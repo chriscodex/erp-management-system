64d4fe64ef9943d27e168d9b9d6e720b1fa19182
Christian <c.espinoza.code@gmail.com>
2024-09-26 10:06:14 -0500

    daly added to utils file

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export async function delay(ms) {
+  try {
+    return new Promise((resolve) => setTimeout(resolve, ms)); // eslint-disable-line
+  } catch (error) {
+    console.log(error);
+  }
+}
