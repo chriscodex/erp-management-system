962e68d61ba7df9aec96ffd42c1d5d53a3255a43
Christian <c.espinoza.code@gmail.com>
2024-09-27 18:26:20 -0500

    error handler added to load users

--- src/app/usuarios/_services/requests.js
+++ src/app/usuarios/_services/requests.js
@@ -3,8 +3,14 @@ import { fetchData } from '@/lib/fetchData';
 
 export async function getAllUsers() {
   try {
-    const users = await fetchData(getAllUsersUrl);
-    return users;
+    const response = await fetchData(getAllUsersUrl);
+    if (response?.status !== 200) {
+      console.log('Error al obtener los usuarios');
+      return { users: [], status: 500 };
+    }
+    console.log(response);
+    const users = response?.data?.payload;
+    return { users, status: 200 };
   } catch (error) {
     console.log(error);
   }
