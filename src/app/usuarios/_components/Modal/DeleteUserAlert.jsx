2cb0fd8934a2fb0312ac283b2b388a3b4521684f
Christian <c.espinoza.code@gmail.com>
2024-09-29 20:05:54 -0500

    deleteUseralert component created

---
+++ src/app/usuarios/_components/Modal/DeleteUserAlert.jsx
@@ -0,0 +1,62 @@
+'use client';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { deleteUser } from '@/app/usuarios/_services/requests';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+
+function DeleteUserAlert({ isOpen, setIsOpen, userDni }) {
+  const router = useRouter();
+
+  const handleConfirmation = async () => {
+    try {
+      setIsOpen(false);
+      toast.promise(deleteUser(userDni), {
+        loading: 'Eliminando...',
+        success: () => {
+          router.refresh();
+          return `Usuario eliminado exitosamente`;
+        },
+        error: (error) => {
+          return error;
+        },
+      });
+    } catch (error) {}
+  };
+
+  return (
+    <>
+      <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+        {/* <AlertDialogTrigger>Open</AlertDialogTrigger> */}
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Estás completamente seguro?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Esta acción no se puede deshacer. Este usuario será
+              permanentemente eliminado y no podrás recuperar sus datos.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={() => setIsOpen(false)}>
+              Cancelar
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={handleConfirmation}>
+              Continuar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+}
+
+export { DeleteUserAlert };
