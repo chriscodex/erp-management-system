7dcb88d9f563618d756b9a761cc95b15819db796
Christian <c.espinoza.code@gmail.com>
2024-10-02 15:41:57 -0500

    marcas icons created

--- src/app/inventario/motos/marcas/page.jsx
+++ src/app/inventario/motos/marcas/page.jsx
@@ -1,8 +1,12 @@
 import { Label } from '@/components/ui/label';
 import { NavbarDynamic } from '@/components/navbar/NavbarDynamic';
+import { Plus, User2Icon } from 'lucide-react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 function MotosMarcasPage() {
-  const titles = [
+  /* Secciones del navbar */
+  const navbarTitles = [
     {
       title: 'Inventario',
       href: '/inventario/todos',
@@ -22,11 +26,22 @@ function MotosMarcasPage() {
 
   return (
     <>
-      <NavbarDynamic titles={titles}>
-        <Label>Motos Marcas</Label>
+      <NavbarDynamic titles={navbarTitles}>
+        <div className="flex justify-between">
+          <div className="flex items-center gap-2">
+            <User2Icon className="h-9 w-9" />
+            <Label className="sm:text-4xl text-xl font-bold">Marcas</Label>
+          </div>
+          <Link href="/usuarios/nuevo" className="flex justify-end">
+            <Button>
+              <Plus />
+              Agregar Nuevo Marca
+            </Button>
+          </Link>
+        </div>
       </NavbarDynamic>
     </>
   );
 }
 
-export default MotosMarcasPage
+export default MotosMarcasPage;
