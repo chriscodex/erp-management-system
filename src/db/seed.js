b997c4e7b07511371b08ca5ee834057cb3f24cef
Christian <c.espinoza.code@gmail.com>
2024-10-02 11:14:05 -0500

    mock data imported on seed

--- src/db/seed.js
+++ src/db/seed.js
@@ -2,164 +2,12 @@ import { connectDB } from '@/db/mongodb';
 import { models } from 'mongoose';
 import bcryptjs from 'bcryptjs';
 
+import { userMockData } from '@/db/mock-data';
 import { User } from '@/backend/users/domain/models/user';
 import { SearchedUser } from '@/backend/searchedUsers/domain/models/searchedUser';
 
 export async function seedUsers() {
   try {
-    const data = [
-      {
-        nombres: 'Christian Gonzalo',
-        apellidos: 'Espinoza Cadillo',
-        dni: '74062106',
-        celular: '931140269',
-        direccion: 'Jr. 9 de diciembre 686 Carhuaz Carhuaz',
-        rol: 'Administrador',
-        password: '123',
-        estado: 'activo',
-      },
-      {
-        nombres: 'Josue Israel',
-        apellidos: 'Rubina Villareal',
-        dni: '44362644',
-        celular: '931140270',
-        direccion: 'Av. Las Flores 364 Independencia Huaraz',
-        rol: 'Administrador',
-        password: '123',
-        estado: 'inactivo',
-      },
-      {
-        nombres: 'Juan Carlos',
-        apellidos: 'Perez Lopez',
-        dni: '51456232',
-        celular: '931140270',
-        direccion: 'Av. Los Próceres 123 Breña Lima',
-        rol: 'Tecnico',
-        password: '123',
-        estado: 'activo',
-      },
-      {
-        nombres: 'Maria Elena',
-        apellidos: 'Gomez Torres',
-        dni: '33148475',
-        celular: '931140271',
-        direccion: 'Calle Las Flores 456 Surco Lima',
-        rol: 'Vendedor',
-        password: '123',
-        estado: 'inactivo',
-      },
-      {
-        nombres: 'Eduardo Israel',
-        apellidos: 'Herrera Figueroa',
-        dni: '25256247',
-        celular: '951625325',
-        direccion: 'Jr. Calle Victoria 123',
-        rol: 'Vendedor',
-        password: '123',
-        estado: 'activo',
-      },
-      {
-        nombres: 'Carlos Alberto',
-        apellidos: 'Lopez Sánchez',
-        dni: '36214789',
-        celular: '987654321',
-        direccion: 'Av. Primavera 456',
-        rol: 'Administrador',
-        password: 'abc123',
-        estado: 'activo',
-      },
-      {
-        nombres: 'Lucía María',
-        apellidos: 'Gómez Pérez',
-        dni: '45123678',
-        celular: '964123789',
-        direccion: 'Calle Las Flores 789',
-        rol: 'Vendedor',
-        password: 'pass456',
-        estado: 'inactivo',
-      },
-      {
-        nombres: 'José Manuel',
-        apellidos: 'Ramírez Díaz',
-        dni: '51247896',
-        celular: '951478632',
-        direccion: 'Jr. Los Pinos 321',
-        rol: 'Vendedor',
-        password: 'xyz789',
-        estado: 'activo',
-      },
-      {
-        nombres: 'María Fernanda',
-        apellidos: 'Castro Rojas',
-        dni: '37451289',
-        celular: '912345678',
-        direccion: 'Av. La Marina 852',
-        rol: 'Supervisor',
-        password: 'super123',
-        estado: 'activo',
-      },
-      {
-        nombres: 'Andrés Felipe',
-        apellidos: 'Torres Ruiz',
-        dni: '48123657',
-        celular: '932147856',
-        direccion: 'Calle Las Palmeras 102',
-        rol: 'Vendedor',
-        password: 'secure789',
-        estado: 'inactivo',
-      },
-      {
-        nombres: 'Sofía Andrea',
-        apellidos: 'Martínez Valdez',
-        dni: '32147895',
-        celular: '984563217',
-        direccion: 'Av. El Sol 654',
-        rol: 'Vendedor',
-        password: 'sofia123',
-        estado: 'activo',
-      },
-      {
-        nombres: 'Juan Carlos',
-        apellidos: 'Paredes Flores',
-        dni: '48956231',
-        celular: '956874123',
-        direccion: 'Jr. San Martín 124',
-        rol: 'Administrador',
-        password: 'admin321',
-        estado: 'activo',
-      },
-      {
-        nombres: 'Elena Patricia',
-        apellidos: 'López Quispe',
-        dni: '57481236',
-        celular: '945612378',
-        direccion: 'Av. La Paz 978',
-        rol: 'Vendedor',
-        password: 'patri456',
-        estado: 'inactivo',
-      },
-      {
-        nombres: 'Ricardo José',
-        apellidos: 'García Huerta',
-        dni: '41257896',
-        celular: '964213785',
-        direccion: 'Calle Los Jazmines 209',
-        rol: 'Supervisor',
-        password: 'ricardo789',
-        estado: 'activo',
-      },
-      {
-        nombres: 'Ana Belén',
-        apellidos: 'Ortega Romero',
-        dni: '35891476',
-        celular: '987124563',
-        direccion: 'Jr. Los Almendros 304',
-        rol: 'Vendedor',
-        password: 'ana456',
-        estado: 'inactivo',
-      },
-    ];
-
     if (User) {
       delete models.User;
     }
@@ -171,7 +19,7 @@ export async function seedUsers() {
     // Hashear las contraseñas de los datos de ejemplo
     /* eslint-disable */
     const hashedData = await Promise.all(
-      data.map(async (user) => {
+      userMockData.map(async (user) => {
         const hashedPassword = await bcryptjs.hash(user.password, 12);
         return { ...user, password: hashedPassword };
       })
