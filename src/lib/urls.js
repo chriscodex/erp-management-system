def18056224509d9a9df577b552ebcb60bd1a425
Christian <c.espinoza.code@gmail.com>
2024-10-03 19:45:27 -0500

    urls moved to lib

---
+++ src/lib/urls.js
@@ -0,0 +1,7 @@
+/* Users */
+export const getAllUsersUrl = `${process.env.NEXT_PUBLIC_API_URL}/users`;
+export const createUserUrl = '/api/users';
+export const updateUserUrl = '/api/users';
+export const deleteUserUrl = '/api/users';
+
+/* Categories */
