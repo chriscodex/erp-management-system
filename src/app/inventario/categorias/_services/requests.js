15345a4f005b2b0907b837e6c4bee73ebc5bdbc1
Christian <c.espinoza.code@gmail.com>
2024-10-03 19:47:53 -0500

    request created for all categories

---
+++ src/app/inventario/categorias/_services/requests.js
@@ -0,0 +1,17 @@
+import { fetchData } from '@/lib/fetchData';
+import { getAllCategoriesUrl } from '@/lib/urls';
+
+export async function getAllCategories() {
+  try {
+    const response = await fetchData(getAllCategoriesUrl);
+    if (response?.status !== 200) {
+      console.log('Error al obtener los usuarios');
+      return { categories: [], status: 500 };
+    }
+    console.log(response);
+    const categories = response?.data?.payload;
+    return { categories, status: 200 };
+  } catch (error) {
+    console.log(error);
+  }
+}
