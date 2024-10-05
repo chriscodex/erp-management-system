5d7e409fcb70867ea6ef7a8d8dbfd88bc2834f23
Christian <c.espinoza.code@gmail.com>
2024-10-04 20:33:29 -0500

    request for userdetail import fixed

--- src/app/usuarios/[userDetail]/_services/requests.js
+++ src/app/usuarios/[userDetail]/_services/requests.js
@@ -1,5 +1,5 @@
 import { fetchData, patchData } from '@/lib/fetchData';
-import { updateUserUrl } from '@/app/usuarios/_services/urls';
+import { updateUserUrl } from '@/lib/urls.js';
 
 export async function getUser(dni) {
   try {
--- src/app/usuarios/_services/urls.js
+++
@@ -1,5 +0,0 @@
-// export const getAllUsersUrl = 'https://6718229fb910c6a6e02b2dae.mockapi.io/users';
-export const getAllUsersUrl = `${process.env.NEXT_PUBLIC_API_URL}/users`;
-export const createUserUrl = '/api/users';
-export const updateUserUrl = '/api/users';
-export const deleteUserUrl = '/api/users';
