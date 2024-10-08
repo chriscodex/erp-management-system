ccbaca4bd403d6d2866980e9a3ac71e024143753
Christian <c.espinoza.code@gmail.com>
2024-10-08 13:54:48 -0500

    category controller logs added

--- src/backend/categorias/infrastructure/controllers.js
+++ src/backend/categorias/infrastructure/controllers.js
@@ -9,18 +9,38 @@ export async function getAllCategoriesController() {
     const categories = await categoryService.getAllCategories();
     return categories;
   } catch (error) {
-    console.error('Error fetching categories:', error);
-    throw new Error('Internal Server Error - getAllCategoriesController');
+    console.error('Controller: Error fetching categories:', error);
+    throw new Error(
+      'Controller: Internal Server Error - getAllCategoriesController'
+    );
+  }
+}
+
+export async function createCategoryController(categoryData) {
+  try {
+    await connectDB();
+
+    /* Responses { payload, status} */
+    const createdCategory = await categoryService.createCategory(categoryData);
+    return createdCategory;
+  } catch (error) {
+    console.error('Controller: Error al crear una categoría:', error);
+    throw new Error(
+      'Controller: Internal Server Error - createCategoryController'
+    );
   }
 }
 
 export async function deleteCategoryController(id) {
   try {
     await connectDB();
+
     const deletedCategory = await categoryService.deleteCategory(id);
     return deletedCategory;
   } catch (error) {
-    console.error('Controller - Error al eliminar categoría:', error);
-    throw new Error('Internal Server Error - deleteCategoryController');
+    console.error('Controller: Error al eliminar categoría:', error);
+    throw new Error(
+      'Controller: Internal Server Error - deleteCategoryController'
+    );
   }
 }
