1111d871e88fb905c6932510b380c16bc5622258
Christian <c.espinoza.code@gmail.com>
2024-10-08 13:14:34 -0500

    POST cateogyr implemented

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import {
   getAllCategoriesController,
+  createCategoryController
 } from '@/backend/categorias/infrastructure/controllers';
 
 export async function GET() {
@@ -15,6 +16,24 @@ export async function GET() {
   } catch (error) {
     return NextResponse.json(
       { message: 'Error obteniendo todas las categorías' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request) {
+  try {
+    const body = await request.json();
+    const { payload, status } = await createCategoryController(body);
+
+    if (status !== 201) {
+      return NextResponse.json({ error: payload }, { status });
+    }
+
+    return NextResponse.json({ payload }, { status });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
       { status: 500 }
     );
   }
