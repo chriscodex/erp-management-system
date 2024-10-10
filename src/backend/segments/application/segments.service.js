e6207582122ccaf9757a92102ce8dc50a05ed16c
Christian <c.espinoza.code@gmail.com>
2024-10-10 18:20:41 -0500

    getAllSegments added to segments service

---
+++ src/backend/segments/application/segments.service.js
@@ -0,0 +1,34 @@
+import { SegmentRepository } from '@/backend/categorias/domain/repositories/categoryRepository.js';
+
+export class SegmentService {
+  constructor() {
+    this.segmentRepository = new SegmentRepository();
+  }
+  async getAllSegments() {
+    try {
+      const categories = await this.segmentRepository.getAllSegments();
+
+      if (!categories) {
+        console.log('Category Service: No se encontraron categorias');
+        return {
+          status: 404,
+          payload: 'No se encontraron categorias',
+        };
+      }
+
+      console.log('Category Service: Categorías encontradas');
+      return {
+        status: 200,
+        payload: categories,
+      };
+    } catch (error) {
+      console.error(
+        `Service: Error interno al obtener todas las categorías: ${error.message}`
+      );
+      return {
+        status: 500,
+        payload: error.message,
+      };
+    }
+  }
+}
