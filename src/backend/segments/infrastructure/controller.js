22c8f3c4bbfd2331e02b444a9237e3df776ef087
Christian <c.espinoza.code@gmail.com>
2024-10-10 18:23:04 -0500

    controller created for segments

---
+++ src/backend/segments/infrastructure/controller.js
@@ -0,0 +1,17 @@
+import { SegmentService } from '@/backend/segments/application/segments.service.js';
+import { connectDB } from '@/db/mongodb';
+
+const segmentService = new SegmentService();
+
+export async function getAllSegmentsController() {
+  try {
+    await connectDB();
+    const categories = await segmentService.getAllSegments();
+    return categories;
+  } catch (error) {
+    console.error('Controller: Error fetching segments:', error);
+    throw new Error(
+      'Controller: Internal Server Error - getAllSegmentsController'
+    );
+  }
+}
