78e845aead25475d7cbbc838bea2bcc648a78173
Christian <c.espinoza.code@gmail.com>
2024-10-10 18:34:34 -0500

    getallsegmentsurl created

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -5,6 +5,7 @@ export const updateUserUrl = '/api/users';
 export const deleteUserUrl = '/api/users';
 
 /* Categories */
+export const getAllSegmentsUrl = `${process.env.NEXT_PUBLIC_API_URL}/segments`;
 export const getAllCategoriesUrl = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 export const createCategoryUrl = '/api/categories';
 export const deleteCategoryUrl = '/api/categories';
