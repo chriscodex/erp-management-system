88b663119f9648a65795cc200178e5685f4147a6
Christian <c.espinoza.code@gmail.com>
2024-10-12 20:46:22 -0500

    page categories now owrks with wraper

--- src/app/inventario/categorias/page.jsx
+++ src/app/inventario/categorias/page.jsx
@@ -13,6 +13,7 @@ import {
   getAllSegments,
 } from '@/app/inventario/categorias/_services/requests';
 import { AddCategory } from './_components/sheets/add-category';
+import { SheetWrapper } from '@/app/inventario/categorias/_components/sheets/sheetWrapper';
 
 export default async function CategoriasPage() {
   const titles = [
@@ -41,15 +42,7 @@ export default async function CategoriasPage() {
             <RiAppsLine className="h-9 w-9" />
             <Label className="sm:text-4xl text-xl font-bold">Categorías</Label>
           </div>
-          <Sheet>
-            <SheetTrigger className="text-start cursor-pointer">
-              <div className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2">
-                <Plus />
-                Agregar Nueva Categoría
-              </div>
-            </SheetTrigger>
-            <AddCategory segments={segments} />
-          </Sheet>
+          <SheetWrapper segments={segments} />
         </div>
         <DataTableCategory
           columns={columnsCategory}
