99397cf7a3aa8718963bbf6c8bf2be7c4e088133
Christian <c.espinoza.code@gmail.com>
2024-10-12 22:00:11 -0500

    updateCategory schema for validation created

---
+++ src/backend/categorias/application/validations/updateCategorySchema.js
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+const objectIdRegex = /^[a-f\d]{24}$/i; // Expresión regular para el formato de ObjectId
+const estados = ['activo', 'inactivo'];
+
+export const updateCategorySchema = z.object({
+  segmentId: z
+    .string()
+    .regex(objectIdRegex, {
+      message: 'El id debe ser un ObjectId válido',
+    })
+    .optional(),
+  nombre: z.string().optional(),
+  descripcion: z.string().optional(),
+  estado: z
+    .enum(estados, {
+      errorMap: () => ({ message: 'Seleccione un estado' }),
+    })
+    .optional(),
+});
