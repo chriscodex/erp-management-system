28de4560580563620472a7a837e56bdfb42ce7ba
Christian <c.espinoza.code@gmail.com>
2024-10-14 16:30:16 -0500

    data-table created for marcasTable

---
+++ src/app/inventario/marcas/_components/MarcasTable/data-table.jsx
@@ -0,0 +1,155 @@
+'use client';
+
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
+} from '@tanstack/react-table';
+import { useEffect, useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+
+import { Input } from '@/components/ui/input';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { DataTablePagination } from '@/components/ui/table-pagination';
+import { DataTableViewOptions } from '@/components/ui/table-view-options';
+import { useIsMobile } from '@/hooks/use-mobile';
+
+export function DataTableMarcas({ columns, data, status = 200 }) {
+  const router = useRouter();
+
+  /* Sorting */
+  const [sorting, setSorting] = useState([]);
+  const [columnFilters, setColumnFilters] = useState([]);
+
+  /* Table */
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+    },
+  });
+
+  /* Search */
+  const [searchValue, setSearchValue] = useState('');
+
+  const TIME_DEBOUNCE = 300;
+
+  const debouncedSearch = useDebouncedCallback((value) => {
+    table.getColumn('apellidos')?.setFilterValue(value);
+  }, TIME_DEBOUNCE);
+
+  useEffect(() => {
+    debouncedSearch(searchValue);
+  }, [searchValue, debouncedSearch]);
+
+  // table.getColumn('rol').getIsVisible();
+
+  useEffect(() => {
+    if (status !== 200) {
+      toast.error(
+        'No podemos conectarnos al servidor en este momento. Verifica tu conexión a internet o inténtalo nuevamente en unos minutos. Si el error persiste, ponte en contacto con Christian.',
+        { duration: 10000 }
+      );
+    }
+  }, [status]);
+
+  useEffect(() => {
+    // Fuerza la actualización de los datos cada vez que se accede a la página
+    router.refresh();
+  }, [router]);
+
+  /* Mobile */
+  const isMobile = useIsMobile();
+  useEffect(() => {
+    if (isMobile) {
+      table.getColumn('rol').toggleVisibility(false);
+      table.getColumn('estado').toggleVisibility(false);
+    }
+  }, [isMobile, table]);
+
+  return (
+    <div>
+      {/* Input */}
+      <div className="flex items-center py-4 w-full">
+        <Input
+          placeholder="Buscar por apellidos"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="max-w-sm"
+        />
+        <DataTableViewOptions table={table} />
+      </div>
+      <div className="rounded-md border sm:min-h-[528px] min-h-[528px] w-auto">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Sin resultados.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <DataTablePagination table={table} />
+    </div>
+  );
+}
