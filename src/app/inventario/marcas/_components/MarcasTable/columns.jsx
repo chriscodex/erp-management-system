1bdd212a3d2a4a35cce4007fc03b99fa2704ed8d
Christian <c.espinoza.code@gmail.com>
2024-10-14 16:29:48 -0500

    columns for marcas created

---
+++ src/app/inventario/marcas/_components/MarcasTable/columns.jsx
@@ -0,0 +1,144 @@
+'use client';
+
+import { MoreHorizontal, ArrowUpDown } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { DeleteUserAlert } from '@/app/usuarios/_components/Dialog/DeleteUserAlert';
+import { useState } from 'react';
+import { Badge } from '@/components/ui/badge';
+
+export const columnsMarcas = [
+  {
+    accessorKey: 'apellidos',
+    header: ({ column }) => {
+      console.log(column);
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Apellidos
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div className="text-start">{row.getValue('apellidos')}</div>;
+    },
+  },
+  {
+    accessorKey: 'nombres',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Nombres
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div className="text-start">{row.getValue('nombres')}</div>;
+    },
+  },
+  {
+    accessorKey: 'rol',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Rol
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: 'estado',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Estado
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <div className="text-start">
+          {row.getValue('estado') === 'activo' ? (
+            <Badge variant="success" className="text-sm">
+              Activo
+            </Badge>
+          ) : (
+            <Badge variant="error" className="text-sm">
+              Inactivo
+            </Badge>
+          )}
+        </div>
+      );
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const { dni } = row.original;
+
+      const router = useRouter();
+
+      const [isOpenDialogDeleteUser, setIsOpenDialogDeleteUser] =
+        useState(false);
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="h-8 w-8 p-0">
+              <span className="sr-only">Abrir menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="center">
+            <DropdownMenuLabel className="select-none">
+              Acciones
+            </DropdownMenuLabel>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => router.push(`/usuarios/${dni}`)}
+            >
+              Detalle
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => setIsOpenDialogDeleteUser(true)}
+            >
+              Eliminar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+          <DeleteUserAlert
+            isOpen={isOpenDialogDeleteUser}
+            setIsOpen={setIsOpenDialogDeleteUser}
+            userDni={dni}
+            actionAfterComplete="refresh"
+          />
+        </DropdownMenu>
+      );
+    },
+  },
+];
