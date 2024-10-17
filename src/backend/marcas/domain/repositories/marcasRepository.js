e2b8f6b555c9377cd4bca7c4b9e39ae4df169eef
Christian <c.espinoza.code@gmail.com>
2024-10-16 21:30:05 -0500

    getAllMarcas created in repository

---
+++ src/backend/marcas/domain/repositories/marcasRepository.js
@@ -0,0 +1,24 @@
+import { Marca } from '@/backend/marcas/domain/models/marca';
+
+export class MarcasRepository {
+  async getAllMarcas() {
+    try {
+      const marcas = await Marca.find();
+
+      if (marcas.length === 0) {
+        console.log('Marca Repository: No se encontraron marcas');
+        return null;
+      }
+
+      console.log('Marca Repository: Marcas encontradas');
+      return marcas;
+    } catch (error) {
+      console.error(
+        `Marca Repository: Error al buscar todas las marcas: ${error.message}`
+      );
+      throw new Error(
+        `Marca Repository: Error al buscar todas las marcas: ${error.message}`
+      );
+    }
+  }
+}
