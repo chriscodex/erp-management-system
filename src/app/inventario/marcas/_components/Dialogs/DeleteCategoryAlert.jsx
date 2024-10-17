32f789385b550349d03445deaf6bba61b6c9262e
Christian <c.espinoza.code@gmail.com>
2024-10-17 13:54:59 -0500

    delteMarca alert implemented

---
+++ src/app/inventario/marcas/_components/Dialogs/DeleteCategoryAlert.jsx
@@ -0,0 +1,88 @@
+'use client';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { deleteMarca } from '@/app/inventario/marcas/_services/requests';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+
+/**
+ * @description Un dialog de confirmación de eliminación de una marca.
+ *
+ * @param {boolean} isOpen - Indica si el diálogo está abierto o no.
+ * @param {(isOpen: boolean) => void} setIsOpen - Función que se llama para cambiar el estado de apertura del dialog.
+ * @param {string} id - ID de la marca a eliminar.
+ * @param {'refresh' | 'push'} actionAfterComplete - Acción a realizar después de eliminar la marca:
+ * - 'refresh': Refrescar la página actual.
+ * - 'push': Redirigir a la ruta '/inventario/marcas'.
+ *
+ * @returns Un JSX con el diálogo de confirmación de eliminación de una marca.
+ */
+export function DeleteMarcaAlert({
+  isOpen,
+  setIsOpen,
+  id,
+  actionAfterComplete,
+}) {
+  const router = useRouter();
+
+  const handleConfirmationDeleteMarca = async () => {
+    try {
+      setIsOpen(false);
+      toast.promise(deleteMarca(id), {
+        loading: 'Eliminando...',
+        success: () => {
+          if (actionAfterComplete === 'refresh') {
+            router.refresh();
+            return `Marca eliminada exitosamente`;
+          }
+          if (actionAfterComplete === 'push') {
+            router.push('/inventario/marcas');
+            return `Marca eliminada exitosamente`;
+          }
+        },
+        error: (error) => {
+          return error;
+        },
+      });
+    } catch (error) {}
+  };
+
+  return (
+    <>
+      <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Estás completamente seguro?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Esta acción no se puede deshacer. Esta marca será permanentemente
+              eliminado y no podrás recuperar sus datos.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel
+              className="bg-secondary text-secondary-foreground hover:opacity-80"
+              onClick={() => setIsOpen(false)}
+            >
+              Cancelar
+            </AlertDialogCancel>
+            <AlertDialogAction
+              className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+              onClick={handleConfirmationDeleteMarca}
+            >
+              Continuar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+}
