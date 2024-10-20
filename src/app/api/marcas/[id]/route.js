5ffe7ca712fab40e036b41db88f8f3e251ca7567
Christian <c.espinoza.code@gmail.com>
2024-10-20 10:30:55 -0500

    update marca implemented in route

--- src/app/api/marcas/[id]/route.js
+++ src/app/api/marcas/[id]/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import {
   getMarcaController,
+  updateMarcaController,
   deleteMarcaController,
 } from '@/backend/marcas/infrastructure/controllers';
 
@@ -17,6 +18,26 @@ export async function GET(request, { params }) {
   } catch (error) {
     return NextResponse.json(
       { message: 'Error obteniendo la marca' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(request, { params }) {
+  try {
+    const { id } = params;
+    const body = await request.json();
+    const { payload, status } = await updateMarcaController(id, body);
+
+    if (status !== 200) {
+      return NextResponse.json({ error: payload }, { status });
+    }
+
+    return NextResponse.json({ payload }, { status });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: 'Error actualizando la marca' },
       { status: 500 }
     );
   }
