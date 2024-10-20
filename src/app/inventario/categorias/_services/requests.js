23fe8eb2b910e4359c8d60e056b2e2259f7eeb96
Christian <c.espinoza.code@gmail.com>
2024-10-19 22:39:41 -0500

    getAllSegments request changed

--- src/app/inventario/categorias/_services/requests.js
+++ src/app/inventario/categorias/_services/requests.js
@@ -21,7 +21,7 @@ export async function getAllCategories() {
   }
 }
 
-export async function getAllSegments() {
+export async function getAllSegmentsRequest() {
   try {
     const response = await fetchData(getAllSegmentsUrl);
     if (response?.status !== 200) {
