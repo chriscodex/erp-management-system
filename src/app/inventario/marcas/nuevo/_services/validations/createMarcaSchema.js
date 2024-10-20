7c6c63c35af97d23cdde33ce808564c921cc48ac
Christian <c.espinoza.code@gmail.com>
2024-10-20 09:30:15 -0500

    validatons name renamed

---
+++ src/app/inventario/marcas/nuevo/_services/validations/createMarcaSchema.js
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+const objectIdRegex = /^[a-f\d]{24}$/i; // Expresión regular para el formato de ObjectId
+
+export const createMarcaSchema = z.object({
+  nombre: z
+    .string()
+    .min(1, {
+      message: 'El nombre debe tener al menos 1 caracter',
+    })
+    .max(50, {
+      message: 'El nombre debe tener menos de 50 caracteres',
+    }),
+  descripcion: z.string().optional(),
+  segmentId: z.string().regex(objectIdRegex, {
+    message: 'Debe elegir un segmento',
+  }),
+});
