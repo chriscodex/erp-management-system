e3fb43a64de5cacfe270e7426c2db9fbc06e9178
Christian <c.espinoza.code@gmail.com>
2024-10-25 11:26:25 -0500

    navbar dynamic fixed responsive

--- src/components/navbar/NavbarDynamic.jsx
+++ src/components/navbar/NavbarDynamic.jsx
@@ -13,7 +13,7 @@ import { Fragment } from 'react';
 function NavbarDynamic({ children, titles = [] }) {
   return (
     <>
-      <SidebarInset>
+      <SidebarInset className="min-w-0">
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
