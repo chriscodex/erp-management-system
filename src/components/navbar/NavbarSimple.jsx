23cde757da56d3e74f25e9ce47a75a9d9418dc7f
Christian <c.espinoza.code@gmail.com>
2024-10-25 11:23:40 -0500

    navbar simple responsive fixed

--- src/components/navbar/NavbarSimple.jsx
+++ src/components/navbar/NavbarSimple.jsx
@@ -10,7 +10,7 @@ import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
 export async function NavbarSimple({ children, title }) {
   return (
     <>
-      <SidebarInset>
+      <SidebarInset className="min-w-0">
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
@@ -27,7 +27,7 @@ export async function NavbarSimple({ children, title }) {
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+        <div>{children}</div>
       </SidebarInset>
     </>
   );
