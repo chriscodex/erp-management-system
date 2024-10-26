3e489647279a31c6473e3b74c577858ec706fc9e
Christian <c.espinoza.code@gmail.com>
2024-10-26 15:18:17 -0500

    colummns for products created

---
+++ src/app/inventario/productos/_components/ProductsTable/columns.jsx
@@ -0,0 +1,144 @@
+'use client';
+
+import { MoreHorizontal, ArrowUpDown, Edit } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Badge } from '@/components/ui/badge';
+import { DeleteMarcaAlert } from '@/app/inventario/marcas/_components/Dialogs/DeleteCategoryAlert.jsx';
+import { RiDeleteBinLine, RiFileListLine } from '@remixicon/react';
+
+export const columnsProducts = [
+  {
+    accessorKey: 'nombre',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Nombre
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div className="text-start">{row.getValue('nombre')}</div>;
+    },
+  },
+  {
+    accessorFn: (row) => row?.segmentId?.nombre,
+    id: 'Segmento',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Segmento
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const segment = row?.original?.segmentId?.nombre;
+      return <div className="text-start">{segment}</div>;
+    },
+  },
+  {
+    accessorKey: 'estado',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Estado
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <div className="text-start">
+          {row.getValue('estado') === 'activo' ? (
+            <Badge variant="successTable" className="text-sm">
+              Activo
+            </Badge>
+          ) : (
+            <Badge variant="error" className="text-sm">
+              Inactivo
+            </Badge>
+          )}
+        </div>
+      );
+    },
+  },
+
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const { _id: id } = row.original;
+
+      const router = useRouter();
+
+      /* Manejar estado de eliminar marca */
+      const [isOpenDialogDeleteUser, setIsOpenDialogDeleteUser] =
+        useState(false);
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="h-8 w-8 p-0">
+              <span className="sr-only">Abrir menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="center">
+            <DropdownMenuLabel className="select-none">
+              Acciones
+            </DropdownMenuLabel>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => router.push(`/inventario/marcas/${id}`)}
+            >
+              <RiFileListLine />
+              Detalle
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => router.push(`/inventario/marcas/${id}/edit`)}
+            >
+              <Edit />
+              Editar
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => setIsOpenDialogDeleteUser(true)}
+            >
+              <RiDeleteBinLine />
+              Eliminar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+          {/* Dialog Delete */}
+          <DeleteMarcaAlert
+            isOpen={isOpenDialogDeleteUser}
+            setIsOpen={setIsOpenDialogDeleteUser}
+            id={id}
+            actionAfterComplete="refresh"
+          />
+        </DropdownMenu>
+      );
+    },
+  },
+];
