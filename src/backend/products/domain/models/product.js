e037a982d546ee746f866bf915251afbbf500966
Christian <c.espinoza.code@gmail.com>
2024-10-28 21:03:38 -0500

    productModel created

---
+++ src/backend/products/domain/models/product.js
@@ -0,0 +1,45 @@
+import { Schema, model, models } from 'mongoose';
+
+import { ProductUnitSchema } from '@/backend/products/domain/models/unidad';
+
+// if (models.Product) {
+//   delete models.Product;
+// }
+
+const productSchema = new Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    nombre: {
+      type: String,
+      required: [true, 'El nombre es requerido en el schema de productos'],
+    },
+    descripcion: {
+      type: String,
+      required: false,
+    },
+    stock: {
+      type: Number,
+      required: [true, 'El stock es requerido en el schema de productos'],
+      min: [0, 'El stock no puede ser negativo'],
+    },
+    stockMinimo: {
+      type: Number,
+      required: [
+        true,
+        'El stock minimo es requerido en el schema de productos',
+      ],
+      min: [0, 'El stock mínimo no puede ser negativo'],
+    },
+    unidades: [ProductUnitSchema],
+  },
+  { timestamps: true }
+);
+
+// Index para mejorar búsquedas por nombre
+productSchema.index({ nombre: 1 });
+
+export const Product = models?.Product || model('Product', productSchema);
