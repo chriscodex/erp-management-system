a4be5710a7e798bb473b2f57ba2f4cf0dc0520bb
Christian <c.espinoza.code@gmail.com>
2024-11-01 16:37:44 -0500

    getallProducts implemented in request

---
+++ src/app/inventario/productos/_services/requests.js
@@ -0,0 +1,16 @@
+import { getAllProductsUrl } from '@/lib/urls';
+import { fetchData } from '@/lib/fetchData';
+
+export async function getAllProductsRequest() {
+  try {
+    const response = await fetchData(getAllProductsUrl);
+    if (response?.status !== 200) {
+      console.log('Error al obtener todos los productos');
+      return { products: [], status: 500 };
+    }
+    const products = response?.data?.payload;
+    return { products, status: 200 };
+  } catch (error) {
+    console.error(error);
+  }
+}
