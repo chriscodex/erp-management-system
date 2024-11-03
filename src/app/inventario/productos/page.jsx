33e1166a39b3b6c8b442c09fcba94f2be874a9da
Christian <c.espinoza.code@gmail.com>
2024-11-03 17:10:06 -0500

    product add button fixed

--- src/app/inventario/productos/page.jsx
+++ src/app/inventario/productos/page.jsx
@@ -2,11 +2,13 @@ import Link from 'next/link';
 import { Plus } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { NavbarDynamic } from '@/components/navbar/NavbarDynamic';
 import { DataTableProducts } from '@/app/inventario/productos/_components/ProductsTable/data-table';
 import { columnsProducts } from '@/app/inventario/productos/_components/ProductsTable/columns';
 import { getAllProductsRequest } from '@/app/inventario/productos/_services/requests';
+import { Label } from '@/components/ui/label';
+import { RiGalleryView2 } from '@remixicon/react';
 
 export default async function ProductsPage() {
   const { products, status } = await getAllProductsRequest();
@@ -29,13 +31,14 @@ export default async function ProductsPage() {
     <NavbarDynamic titles={navbarTitles}>
       <div className="container mx-auto p-6">
         <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-2xl font-bold">
-              Inventario de Productos
-            </CardTitle>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 gap-2">
+            <div className="flex items-center gap-2">
+              <RiGalleryView2 className="md:h-9 h-5 md:w-9 w-5" />
+              <Label className="sm:text-4xl text-xl font-bold">Productos</Label>
+            </div>
             <Button asChild>
               <Link href="/inventario/productos/nuevo">
-                <Plus className="mr-2 h-4 w-4" /> Agregar Producto
+                <Plus className="h-4 w-4" /> Agregar Producto
               </Link>
             </Button>
           </CardHeader>
