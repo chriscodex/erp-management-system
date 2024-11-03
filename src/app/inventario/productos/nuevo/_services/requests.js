2e9aaa66f1355f4b2958f7147c22e719742f8ef5
Christian <c.espinoza.code@gmail.com>
2024-11-03 17:53:18 -0500

    requests cahanged for nuevo

--- src/app/inventario/productos/nuevo/_services/requests.js
+++ src/app/inventario/productos/nuevo/_services/requests.js
@@ -1,12 +1,16 @@
-import { getAllMarcasUrlServer, getAllCategoriesUrlServer } from '@/lib/urls';
+import {
+  getAllMarcasServerUrl,
+  getAllCategoriesServerUrl,
+  getAllProveedoresServerUrl,
+} from '@/lib/urls';
 import { fetchData } from '@/lib/fetchData';
 
 export async function getAllMarcasRequest() {
   try {
-    const response = await fetchData(getAllMarcasUrlServer);
+    const response = await fetchData(getAllMarcasServerUrl);
     if (response?.status !== 200) {
       console.log('Error al obtener todas las marcas');
-      return { marcas: [], status: 500 };
+      return { marcas: [], status: response?.status };
     }
     const marcas = response?.data?.payload;
     return { marcas, status: 200 };
@@ -17,13 +21,27 @@ export async function getAllMarcasRequest() {
 
 export async function getAllCategoriesRequest() {
   try {
-    const response = await fetchData(getAllCategoriesUrlServer);
+    const response = await fetchData(getAllCategoriesServerUrl);
     if (response?.status !== 200) {
       console.log('Error al obtener todas las categorias');
-      return { categories: [], status: 500 };
+      return { categories: [], status: response?.status };
     }
     const categories = response?.data?.payload;
     return { categories, status: 200 };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getAllProveedoresRequest() {
+  try {
+    const response = await fetchData(getAllProveedoresServerUrl);
+    if (response?.status !== 200) {
+      console.log('Error al obtener todas los proveedores');
+      return { proveedores: [], status: response?.status };
+    }
+    const proveedores = response?.data?.payload;
+    return { proveedores, status: 200 };
   } catch (error) {
     console.error(error);
   }
