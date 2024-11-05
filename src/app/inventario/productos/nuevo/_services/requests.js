4f177590e1e1278fa765bf0df3862ff05ab145e7
Christian <c.espinoza.code@gmail.com>
2024-11-05 13:54:01 -0500

    getMarca in addProduct formulary logic implmeneted

--- src/app/inventario/productos/nuevo/_services/requests.js
+++ src/app/inventario/productos/nuevo/_services/requests.js
@@ -1,9 +1,28 @@
 import {
   getAllMarcasServerUrl,
+  getMarcasBySegmenteIdServerUrl,
   getAllCategoriesServerUrl,
   getAllProveedoresServerUrl,
+  getAllSegmentsServerUrl,
+  getSegmentsByFilterServerUrl,
 } from '@/lib/urls';
 import { fetchData } from '@/lib/fetchData';
+
+export async function getMarcasBySegmentIdRequest(segmentId) {
+  try {
+    const response = await fetchData(
+      `${getMarcasBySegmenteIdServerUrl}/?segmentId=${segmentId}`
+    );
+    if (response?.status !== 200) {
+      console.log('Error al obtener marcas por segmento');
+      return { marcas: [], status: response?.status };
+    }
+    const marcas = response?.data?.payload;
+    return { marcas, status: 200 };
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export async function getAllMarcasRequest() {
   try {
@@ -42,6 +61,35 @@ export async function getAllProveedoresRequest() {
     }
     const proveedores = response?.data?.payload;
     return { proveedores, status: 200 };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getAllSegmentsRequest() {
+  try {
+    const response = await fetchData(getAllSegmentsServerUrl);
+    if (response?.status !== 200) {
+      console.log('Error al obtener todos los segmentos');
+      return { segments: [], status: response?.status };
+    }
+    const segments = response?.data?.payload;
+    return { segments, status: 200 };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getSegmentsByFilterRequest(filter) {
+  try {
+    const url = `${getSegmentsByFilterServerUrl}?nombre=${filter?.nombre}`;
+    const response = await fetchData(url);
+    if (response?.status !== 200) {
+      console.log('Error al obtener el segmento');
+      return { segment: null, status: response?.status };
+    }
+    const segment = response?.data?.payload;
+    return { segment, status: 200 };
   } catch (error) {
     console.error(error);
   }
--- src/app/inventario/productos/nuevo/page.jsx
+++ src/app/inventario/productos/nuevo/page.jsx
@@ -1,14 +1,26 @@
 import {
-  getAllMarcasRequest,
+  getMarcasBySegmentIdRequest,
   getAllCategoriesRequest,
   getAllProveedoresRequest,
+  getSegmentsByFilterRequest,
 } from '@/app/inventario/productos/nuevo/_services/requests';
 import { FormAddProduct } from '@/app/inventario/productos/nuevo/_components/FormAddProduct';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 export default async function AddProductPage() {
+  const { segment, status } = await getSegmentsByFilterRequest({
+    nombre: 'Productos',
+  });
+
   const [marcasResponse, categoriesResponse, proveedoresResponse] =
     await Promise.all([
-      getAllMarcasRequest(),
+      getMarcasBySegmentIdRequest(segment?._id),
       getAllCategoriesRequest(),
       getAllProveedoresRequest(),
     ]);
@@ -17,7 +29,23 @@ export default async function AddProductPage() {
   const { categories } = categoriesResponse;
   const { proveedores } = proveedoresResponse;
 
-  const segment = 'Productos'
-
-  return <FormAddProduct />;
+  return (
+    <div className="container mx-auto p-6">
+      <Card className="w-full max-w-7xl mx-auto">
+        <CardHeader>
+          <CardTitle className="text-2xl">Agregar Producto</CardTitle>
+          <CardDescription>
+            Complete los detalles del nuevo producto a continuación.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <FormAddProduct
+          marcas={marcas}
+          categories={categories}
+          proveedores={proveedores}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
