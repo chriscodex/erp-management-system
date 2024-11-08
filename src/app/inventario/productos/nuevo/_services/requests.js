152acb695d38cbb93482af53bdaeca842433f65c
Christian <c.espinoza.code@gmail.com>
2024-11-08 13:11:34 -0500

    getAllAlmacenesRequest implemented in createnewUser

--- src/app/inventario/productos/nuevo/_services/requests.js
+++ src/app/inventario/productos/nuevo/_services/requests.js
@@ -3,6 +3,7 @@ import {
   getCategoriesBySegmentDataServerUrl,
   getAllProveedoresServerUrl,
   getSegmentsByFilterServerUrl,
+  getAllAlmacenesServerUrl,
 } from '@/lib/urls';
 import { fetchData } from '@/lib/fetchData';
 
@@ -81,6 +82,20 @@ export async function getAllProveedoresRequest() {
     }
     const proveedores = response?.data?.payload;
     return { proveedores, status: 200 };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getAllAlmacenesRequest() {
+  try {
+    const response = await fetchData(getAllAlmacenesServerUrl);
+    if (response?.status !== 200) {
+      console.log('Error al obtener todos los almacenes');
+      return { almacenes: [], status: 500 };
+    }
+    const almacenes = response?.data?.payload;
+    return { almacenes, status: 200 };
   } catch (error) {
     console.error(error);
   }
