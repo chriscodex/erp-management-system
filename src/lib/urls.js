6e3f2560a067aea34f50e0640474f0a0828a89f2
Christian <c.espinoza.code@gmail.com>
2024-11-08 13:11:10 -0500

    getallAlmacenes url renamed

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -24,7 +24,7 @@ export const updateMarcaUrl = '/api/marcas';
 export const deleteMarcaUrl = '/api/marcas';
 
 /* Almacen */
-export const getAllAlmacenesUrl = `${process.env.NEXT_PUBLIC_API_URL}/almacenes`;
+export const getAllAlmacenesServerUrl = `${process.env.NEXT_PUBLIC_API_URL}/almacenes`;
 export const getAlmacenUrl = `${process.env.NEXT_PUBLIC_API_URL}/almacenes`;
 
 /* Products */
