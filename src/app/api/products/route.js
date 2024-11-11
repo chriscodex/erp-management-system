09601bf8470253d2a1d05ebe274873b290681e97
Christian <c.espinoza.code@gmail.com>
2024-11-11 09:58:18 -0500

    createProduct implemented in route.js

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server';
 
-import { getAllProductsController } from '@/backend/products/infrastructure/controllers';
+import {
+  getAllProductsController,
+  createProductController,
+} from '@/backend/products/infrastructure/controllers';
 
 export async function GET() {
   try {
@@ -17,6 +20,24 @@ export async function GET() {
     );
     return NextResponse.json(
       { message: 'Error obteniendo todos los products' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request) {
+  try {
+    const body = await request.json();
+    const { payload, status } = await createProductController(body);
+
+    if (status !== 201) {
+      return NextResponse.json({ error: payload }, { status });
+    }
+
+    return NextResponse.json({ payload }, { status });
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
       { status: 500 }
     );
   }
