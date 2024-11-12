f054e3b3f5b98a563d2885ca849687392d503431
Christian <c.espinoza.code@gmail.com>
2024-11-12 16:25:35 -0500

    populated in productRepository added after create

--- src/backend/products/domain/repositories/productRepository.js
+++ src/backend/products/domain/repositories/productRepository.js
@@ -74,7 +74,12 @@ export class ProductRepository {
       const savedProduct = await newProduct.save();
 
       // Populamos el campo segmentId después de guardar
-      const populatedProduct = await savedProduct.populate('segmentId');
+      const populatedProduct = await savedProduct
+        .populate('segmentId')
+        .populate('marcaId')
+        .populate('categoriaId')
+        .populate('almacenId')
+        .populate('proveedorId');
 
       console.log('Product Repository: Product creada correctamente');
       return populatedProduct;
