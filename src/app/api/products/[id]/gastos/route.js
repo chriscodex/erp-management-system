356d4e99b7a14deb0cde6f0482422fadaf379357
Christian <c.espinoza.code@gmail.com>
2024-12-06 11:13:17 -0500

    createGasto api added

---
+++ src/app/api/products/[id]/gastos/route.js
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+import { createGastoController } from '@/backend/products/infrastructure/controllers';
+
+export async function POST(request, contextRoute) {
+  try {
+    const { payload, status } = await createGastoController(request, contextRoute);
+
+    if (status !== 201) {
+      return NextResponse.json({ error: payload }, { status });
+    }
+
+    return NextResponse.json({ payload }, { status });
+  } catch (error) {
+    console.error(
+      `Products Route: Error interno al crear el gasto: ${error.message}`
+    );
+    return NextResponse.json(
+      { error: 'Error interno al crear el gasto' },
+      { status: 500 }
+    );
+  }
+}
