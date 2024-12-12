5384dfabd5864667406dd51906e9b2d3f45c2211
Christian <c.espinoza.code@gmail.com>
2024-12-11 22:37:53 -0500

    test 123

--- src/backend/products/domain/repositories/gastoRepository.js
+++ src/backend/products/domain/repositories/gastoRepository.js
@@ -32,4 +32,30 @@ export class GastoRepository {
       );
     }
   }
+  async createGasto(gastoData, productId) {
+    try {
+      const updatedMarca = await this.productModel.findOneAndUpdate(
+        { _id: new mongoose.Types.ObjectId(productId) },
+        { $push: { gastos: gastoData } },
+        {
+          new: true,
+        }
+      );
+
+      if (!updatedMarca) {
+        console.log('Product Repository: Producto no encontrado');
+        return null;
+      }
+
+      console.log('Product Repository: Gasto agregado correctamente');
+      return updatedMarca;
+    } catch (error) {
+      console.error(
+        `Product Repository: Error al agregar un gasto al producto: ${error.message}`
+      );
+      throw new Error(
+        `Error al agregar un gasto al producto: ${error.message}`
+      );
+    }
+  }
 }
