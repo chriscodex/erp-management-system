ec59d8480f671c7044ac69d9472002ec68f3eb48
Christian <c.espinoza.code@gmail.com>
2024-12-28 09:26:22 -0500

    estado añadido al modelo table

--- src/app/inventario/motos/modelos/_components/ModelosTable/columns.jsx
+++ src/app/inventario/motos/modelos/_components/ModelosTable/columns.jsx
@@ -50,7 +50,6 @@ export const columnsModelos = [
       );
     },
     cell: ({ row }) => {
-      console.log(row.original);
       const marca = row?.original?.marcaId?.nombre;
       return <div className="text-start">{marca}</div>;
     },
@@ -108,6 +107,39 @@ export const columnsModelos = [
       );
 =======
 >>>>>>> e4ed9ad (marcas modelos table added)
+    },
+  },
+  {
+    accessorFn: (row) => row?.estado,
+    id: 'estado',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Estado
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return (
+        <div className="text-start">
+          {row.getValue('estado') === 'activo' ? (
+            <Badge
+              variant="successTable"
+              className="text-sm w-[71px] flex justify-center"
+            >
+              Activo
+            </Badge>
+          ) : (
+            <Badge variant="error" className="text-sm">
+              Inactivo
+            </Badge>
+          )}
+        </div>
+      );
     },
   },
   {
