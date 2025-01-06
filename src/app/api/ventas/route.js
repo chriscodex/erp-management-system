2faa5878522d08c611520fb3c7de516cd0f81d35
Christian <c.espinoza.code@gmail.com>
2025-01-06 15:43:21 -0500

    create venta implemented in route

---
+++ src/app/api/ventas/route.js
@@ -0,0 +1,22 @@
+import { createVentaController } from '@/backend/ventas/infrastructure/controllers';
+import { NextResponse } from 'next/server';
+
+export async function POST(request) {
+  try {
+    const { payload, status } = await createVentaController(request);
+
+    if (status !== 201) {
+      return NextResponse.json({ error: payload }, { status });
+    }
+
+    return NextResponse.json({ payload }, { status });
+  } catch (error) {
+    console.error(
+      `Ventas Route: Error interno al crear la venta: ${error.message}`
+    );
+    return NextResponse.json(
+      { error: 'Error interno al crear la venta' },
+      { status: 500 }
+    );
+  }
+}
