f8d8f29b3f9a73cdd3be400f396a230aa59c6608
Christian <c.espinoza.code@gmail.com>
2025-01-07 18:07:13 -0500

    put method api preventa

--- src/app/api/preventas/[preventaId]/route.js
+++ src/app/api/preventas/[preventaId]/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import {
   deletePreventaController,
+  updatePreventaController,
 } from '@/backend/preventas/infrastructure/controllers';
 
 export async function DELETE(_, contextRoute) {
@@ -18,6 +19,29 @@ export async function DELETE(_, contextRoute) {
     );
     return NextResponse.json(
       { error: 'Error interno eliminando la preventa' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(request, contextRoute) {
+  try {
+    const { payload, status } = await updatePreventaController(
+      request,
+      contextRoute
+    );
+
+    if (status !== 201) {
+      return NextResponse.json({ error: payload }, { status });
+    }
+
+    return NextResponse.json({ payload }, { status });
+  } catch (error) {
+    console.error(
+      `Preventa Route: Error interno al actualizar la preventa: ${error.message}`
+    );
+    return NextResponse.json(
+      { message: 'Error interno al actualizar la preventa' },
       { status: 500 }
     );
   }
