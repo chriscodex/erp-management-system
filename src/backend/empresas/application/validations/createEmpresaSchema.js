72537085d8711fff92848f2f61c31448f8e60617
Christian <c.espinoza.code@gmail.com>
2025-08-15 18:36:41 -0500

    refactor: Actualizar createEmpresaSchema para hacer campos opcionales y mejorar la validación

--- src/backend/empresas/application/validations/createEmpresaSchema.js
+++ src/backend/empresas/application/validations/createEmpresaSchema.js
@@ -12,12 +12,10 @@ export const createEmpresaSchema = z.object({
     }),
   descripcion: z
     .string()
-    .min(3, {
-      message: 'La descripción debe tener al menos 3 caracteres',
-    })
     .max(250, {
       message: 'La descripción no puede tener más de 250 caracteres',
-    }),
+    })
+    .optional(),
   direccion: z
     .string()
     .min(3, {
@@ -26,8 +24,30 @@ export const createEmpresaSchema = z.object({
     .max(150, {
       message: 'La dirección no puede tener más de 150 caracteres',
     }),
+  departamento: z
+    .string({
+      required_error: 'El departamento es requerido',
+    })
+    .min(2, { message: 'El departamento debe tener al menos 2 caracteres' }),
+  distrito: z
+    .string({
+      required_error: 'El distrito es requerido',
+    })
+    .min(2, { message: 'El distrito debe tener al menos 2 caracteres' }),
+  provincia: z
+    .string({
+      required_error: 'La provincia es requerida',
+    })
+    .min(2, { message: 'La provincia debe tener al menos 2 caracteres' }),
+  ubigeo: z
+    .string({
+      required_error: 'El ubigeo es requerido',
+    })
+    .length(6, { message: 'El ubigeo debe tener 6 dígitos' }),
   telefono: z
-    .string()
-    .min(4, { message: 'El teléfono debe tener al menos 4 dígitos' }),
-  email: z.string().email({ message: 'Ingrese un correo válido' }),
+    .string({
+      required_error: 'El teléfono es requerido',
+    })
+    .optional(),
+  email: z.string().optional(),
 });
